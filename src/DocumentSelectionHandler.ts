012ed957eb073d0c7afd887476b77ee781236cca
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-03-22 13:51:53 +0000

    wip

--- src/DocumentSelectionHandler.ts
+++ src/DocumentSelectionHandler.ts
@@ -1,46 +1,35 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { LambdaInterface } from "@aws-lambda-powertools/commons";
-import { Metrics } from "@aws-lambda-powertools/metrics";
 import { Logger } from "@aws-lambda-powertools/logger";
+import { Metrics } from "@aws-lambda-powertools/metrics";
+// import { Response } from "./utils/Response";
+// import { ResourcesEnum } from "./models/enums/ResourcesEnum";
+// import { AppError } from "./utils/AppError";
+// import { HttpCodesEnum } from "./utils/HttpCodesEnum";
+import { LambdaInterface } from "@aws-lambda-powertools/commons";
+// import { HttpVerbsEnum } from "./utils/HttpVerbsEnum";
 import { Constants } from "./utils/Constants";
-import { ResourcesEnum } from "./models/enums/ResourcesEnum";
-import { Response } from "./utils/Response";
-import { HttpCodesEnum } from "./utils/HttpCodesEnum";
-import { AppError } from "./utils/AppError";
 import { DocumentSelectorProcessor } from "./services/DocumentSelectorProcessor";
 
 const POWERTOOLS_METRICS_NAMESPACE = process.env.POWERTOOLS_METRICS_NAMESPACE ? process.env.POWERTOOLS_METRICS_NAMESPACE : Constants.F2F_METRICS_NAMESPACE;
-const POWERTOOLS_LOG_LEVEL = process.env.POWERTOOLS_LOG_LEVEL ? process.env.POWERTOOLS_LOG_LEVEL : "DEBUG";
-const POWERTOOLS_SERVICE_NAME = process.env.POWERTOOLS_SERVICE_NAME ? process.env.POWERTOOLS_SERVICE_NAME : Constants.DOCUMENT_SELECTION_LOGGER_SVC_NAME;
+const POWERTOOLS_LOG_LEVEL = process.env.POWERTOOLS_LOG_LEVEL ? process.env.POWERTOOLS_LOG_LEVEL : Constants.DEBUG;
+const POWERTOOLS_SERVICE_NAME = process.env.POWERTOOLS_SERVICE_NAME ? process.env.POWERTOOLS_SERVICE_NAME : Constants.AUTHORIZATIONCODE_LOGGER_SVC_NAME;
+
 const logger = new Logger({
 	logLevel: POWERTOOLS_LOG_LEVEL,
 	serviceName: POWERTOOLS_SERVICE_NAME,
 });
 
-const metrics = new Metrics({ namespace: POWERTOOLS_METRICS_NAMESPACE });
+const metrics = new Metrics({ namespace: POWERTOOLS_METRICS_NAMESPACE, serviceName: POWERTOOLS_SERVICE_NAME });
 
-export class DocumentSelection implements LambdaInterface {
+class AuthorizationCodeHandler implements LambdaInterface {
 
 	@metrics.logMetrics({ throwOnEmptyMetrics: false, captureColdStartMetric: true })
 	async handler(event: APIGatewayProxyEvent, context: any): Promise<APIGatewayProxyResult> {
-		console.log('event', event);
-		return await DocumentSelectorProcessor.getInstance(logger, metrics).processRequest(event);
-		switch (event.resource) {
-			case ResourcesEnum.DOCUMENTSELECTION:
-				try {
-					logger.info("Got token request:", { event });
-					return await DocumentSelectorProcessor.getInstance(logger, metrics).processRequest(event);
-				} catch (err) {
-					logger.error({ message: "An error has occurred. ", err });
-					return new Response(HttpCodesEnum.SERVER_ERROR, "An error has occurred");
-				}
-				return new Response(HttpCodesEnum.NOT_FOUND, "");
-
-			default:
-				throw new AppError("Requested resource does not exist" + { resource: event.resource }, HttpCodesEnum.NOT_FOUND);
-
+			console.log('event', event);
+			return await DocumentSelectorProcessor.getInstance(logger, metrics).processRequest(event);
 		}
 	}
+
 }
-const handlerClass = new DocumentSelection();
+const handlerClass = new AuthorizationCodeHandler();
 export const lambdaHandler = handlerClass.handler.bind(handlerClass);
