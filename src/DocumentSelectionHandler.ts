81d977ec24aa1d90035929fe559ed46fabc462cb
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-03-22 17:50:11 +0000

    wip

--- src/DocumentSelectionHandler.ts
+++ src/DocumentSelectionHandler.ts
@@ -1,18 +1,18 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { Logger } from "@aws-lambda-powertools/logger";
 import { Metrics } from "@aws-lambda-powertools/metrics";
-// import { Response } from "./utils/Response";
-// import { ResourcesEnum } from "./models/enums/ResourcesEnum";
-// import { AppError } from "./utils/AppError";
-// import { HttpCodesEnum } from "./utils/HttpCodesEnum";
+import { Response } from "./utils/Response";
+import { ResourcesEnum } from "./models/enums/ResourcesEnum";
+import { AppError } from "./utils/AppError";
+import { HttpCodesEnum } from "./utils/HttpCodesEnum";
 import { LambdaInterface } from "@aws-lambda-powertools/commons";
-// import { HttpVerbsEnum } from "./utils/HttpVerbsEnum";
+import { HttpVerbsEnum } from "./utils/HttpVerbsEnum";
 import { Constants } from "./utils/Constants";
-import { DocumentSelectorProcessor } from "./services/DocumentSelectorProcessor";
+import { DocumentSelectionRequestProcessor } from "./services/DocumentSelectionRequestProcessor";
 
 const POWERTOOLS_METRICS_NAMESPACE = process.env.POWERTOOLS_METRICS_NAMESPACE ? process.env.POWERTOOLS_METRICS_NAMESPACE : Constants.F2F_METRICS_NAMESPACE;
 const POWERTOOLS_LOG_LEVEL = process.env.POWERTOOLS_LOG_LEVEL ? process.env.POWERTOOLS_LOG_LEVEL : Constants.DEBUG;
-const POWERTOOLS_SERVICE_NAME = process.env.POWERTOOLS_SERVICE_NAME ? process.env.POWERTOOLS_SERVICE_NAME : Constants.AUTHORIZATIONCODE_LOGGER_SVC_NAME;
+const POWERTOOLS_SERVICE_NAME = process.env.POWERTOOLS_SERVICE_NAME ? process.env.POWERTOOLS_SERVICE_NAME : Constants.DOCUMENT_SELECTION_LOGGER_SVC_NAME;
 
 const logger = new Logger({
 	logLevel: POWERTOOLS_LOG_LEVEL,
@@ -25,9 +25,8 @@ class AuthorizationCodeHandler implements LambdaInterface {
 
 	@metrics.logMetrics({ throwOnEmptyMetrics: false, captureColdStartMetric: true })
 	async handler(event: APIGatewayProxyEvent, context: any): Promise<APIGatewayProxyResult> {
-			console.log('event', event);
-			return await DocumentSelectorProcessor.getInstance(logger, metrics).processRequest(event);
-		}
+			console.log('here');
+			return await DocumentSelectionRequestProcessor.getInstance(logger, metrics).processRequest();
 	}
 
 }
