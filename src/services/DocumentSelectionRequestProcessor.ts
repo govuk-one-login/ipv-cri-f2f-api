d4d495f4350fac5ac3f98d8b225eaee7c5480b8f
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-03-24 00:11:44 +0000

    wip

--- src/services/DocumentSelectionRequestProcessor.ts
+++ src/services/DocumentSelectionRequestProcessor.ts
@@ -3,43 +3,93 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 import { Logger } from "@aws-lambda-powertools/logger";
 import { YotiService } from "./YotiService";
 import axios from "axios";
-import https from 'https'
+import https from "https";
 
 export class DocumentSelectionRequestProcessor {
-	private static instance: DocumentSelectionRequestProcessor;
+  private static instance: DocumentSelectionRequestProcessor;
 
-	private readonly logger: Logger;
+  private readonly logger: Logger;
 
-	private readonly metrics: Metrics;
+  private readonly metrics: Metrics;
 
-	private readonly yotiService: YotiService;
+  private readonly yotiService: YotiService;
 
-	constructor(logger: Logger, metrics: Metrics) {
-		this.logger = logger;
-		this.metrics = metrics;
-		this.yotiService = YotiService.getInstance(this.logger);
-	}
+  constructor(logger: Logger, metrics: Metrics) {
+    this.logger = logger;
+    this.metrics = metrics;
+    this.yotiService = YotiService.getInstance(
+      this.logger,
+      "1f9edc97-c60c-40d7-becb-c1c6a2ec4963",
+      Buffer.from(
+        '"LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQ0KTUlJRW93SUJBQUtDQVFFQXVPengvUE5IWUZjVDI5SEozWkk4eUVscVhGa3p6QzlQVnVXZkNVSmg1TDlzeml6TA0KSndadmNtWm9UdnJKd0JwdVRuUktOOGQ2cGVOdG1iVW5NVUVwOFArUCtTUFJ0Nm14NEt4S3c4NGZyaU9KVldRMw0KRnp0YzQzZDRPdDRWWnVsM2dLMDNpWjE5OWpIS09TY3pQR0lIOFVHbTBtdG5pQjY1RWNVbzBCcXliTkFydkZ4Mg0KbGY0NGdXZFlXa3pxN1JlWXJjV0IzUnQyZW9UQ29OSHB1b1BCMmxheDdLTGpaSW8yQnI2a2hmNXpHZFk2TlZ3Yw0KV2lqUXVrZHhBZzFXODQ3djVGM3ZxZiswYTdtdy96dXp4UE84b2grbCtvTlZGWnV6OFdRcE5GOGpYSmJqSjJtdw0KdDJlZENlRjhPdHE5aXVvZ0FwbTdHVkM5NnRGY2dZOHdjMzRCbHdJREFRQUJBb0lCQUVSRkFWODdadjNibDlLKw0KdzlIL1UxNHpzRWJ3alkxVForeHlNQURJbDRCTjVUckdVYWVTR2lHM1Njbmlqd25UR0NieGlwYkdGM0xYbjlZdg0KaVEwSGZCYmpHa0ZGWm41eDhOKzdhNHlXODU0anMyY3BiS2N3ZkszakpLbEM1TTdONFJiSm0rZTdrMDJJK3htWA0KYmJSY2tvY1FFQ2k5aXdhM0pPYTVEVVNxbm5WSjBqTGlFYkcyd2dMR1NMUE5XQjdNTG1tU2ZLb1dMMHFhUTFUbA0KQUdBM0ExU1JhQ21HbHpEcUZpNEhzQXFTOWcvY2FuKzlsbHMwLzlrUGJYbTgvTVI0TkV5NlNLTkQrVG45NXA0TA0KV29UN0RWUUhXUm5hSnNwT0RGZEdONi9EdlJDcTRPcjFBZ2dta2hSNUY2OGwveDZ1VmYrdVhIZWpsai9tdUs1Wg0KVlRWYmFzMENnWUVBM21vcElsV3VoMllSVGpLRVYvS3J6Uk5LNFdLODR3MktvYzdpbENxSFptM1g4Z29VY1lYWg0KQVdDUkxwbk5BNjM3bE40WFdBUGpzWUh2SVZiSDZqTkZJVXVVWjBvSDZKM0ptQWwyNnJ4RDF1MHlMV3ptVnpjZw0KYVQ3SlB2a3N6R01RajVQNnplbFpLeDNjRlhEYVpOUzQwOUdFTDJNRFhibVRYbElFbldXMEJJTUNnWUVBMU5tVA0KUVVBTUdwVVNXK3gvaE4zM1BvcVBvMWc3VVBiQzNXR0RzUk1TN1Axclg1cko1dWZGNHBPM0hNRWxCYnI3MUQvdQ0KZG5GRXpIU2R3amk0K3B3azZlN3ZxWmZZTjZxcm15ZE1XNStydFpMRDZGN0UwOHRsTlRjS1lxWEdFb01LU20rTQ0KZXh3NHpFaFR3eHJpb0k4OVNLMitwZjFsRlY2WStqT0ZBM2dEeWwwQ2dZQTl4T3dCbmh3bmZHQklBRjJpODBGbA0KWmZ0QTF5UXdScjErcks5ZWlPVXJ2RXZoNjR5NzdubFRHWmVZZ3B4ZWJqNVZuTXlNNUIralY3dXEzYWdOMGdsYg0KOHRxWFNRY2pRbGNQZzJsa1RiN2xmT0NYbnd2bG91aThjZlNBS2NHWVRkYjJtUmxwZ0dvRVFIbXo5ckNnbWpkdQ0KbEZ5S3NJdnB4clUzMTcxeVhTRitzd0tCZ0Nnci9FZlhYN0Z3cXl0Z1FsMEFTNWFwTGh6bEhTVFZ2Y2RzalI5Ug0KZTBQeFBGK3A1aXRIV1NpL09sTVovUFBNNjVTT2tHSVlWS1RGNmJScllqWWlYREV1b0ZzQXhwVVRDQnBiVzJUNw0KaUx2WFBGUlI5cElHbEU1cjJmRHVXcWpKeEpweDNUQ3JEZWs0U1Y3TVp6Tm9kV2VQS3lsRzN0b2VwWDZyVkpKbw0KQjBjZEFvR0JBSlBZY0lSdVVjRUE0T3NMTDYrQnZwbldVTFZQQUNPY3I3Z0dkOTg3bExJNm9DemQ0ajJWZ1ZTNQ0KQ1kvbkRXQ2lvZTFWbElab3ptQzFxdDF6dTg3ZE1WdS8vNGhRNklDYUQ1K2VIMnlQTVRuYmVmZVVESTRqVklFVg0KU21SSmo5YVVHWjRHWXd2TnhPbmtjeTI2eGs3RkpqbW12clJIK2FHRXNqZzVCWkxxUlAwdA0KLS0tLS1FTkQgUlNBIFBSSVZBVEUgS0VZLS0tLS0="',
+        "base64"
+      ).toString("utf8")
+    );
+  }
 
-	static getInstance(logger: Logger, metrics: Metrics): DocumentSelectionRequestProcessor {
-		if (!DocumentSelectionRequestProcessor.instance) {
-			DocumentSelectionRequestProcessor.instance = new DocumentSelectionRequestProcessor(logger, metrics);
-		}
-		return DocumentSelectionRequestProcessor.instance;
-	}
+  static getInstance(
+    logger: Logger,
+    metrics: Metrics
+  ): DocumentSelectionRequestProcessor {
+    if (!DocumentSelectionRequestProcessor.instance) {
+      DocumentSelectionRequestProcessor.instance =
+        new DocumentSelectionRequestProcessor(logger, metrics);
+    }
+    return DocumentSelectionRequestProcessor.instance;
+  }
 
-	async processRequest() {
-		const CLIENT_SDK_ID = '1f9edc97-c60c-40d7-becb-c1c6a2ec4963';
-		const PEM_KEY = Buffer.from('"LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQ0KTUlJRW93SUJBQUtDQVFFQXVPengvUE5IWUZjVDI5SEozWkk4eUVscVhGa3p6QzlQVnVXZkNVSmg1TDlzeml6TA0KSndadmNtWm9UdnJKd0JwdVRuUktOOGQ2cGVOdG1iVW5NVUVwOFArUCtTUFJ0Nm14NEt4S3c4NGZyaU9KVldRMw0KRnp0YzQzZDRPdDRWWnVsM2dLMDNpWjE5OWpIS09TY3pQR0lIOFVHbTBtdG5pQjY1RWNVbzBCcXliTkFydkZ4Mg0KbGY0NGdXZFlXa3pxN1JlWXJjV0IzUnQyZW9UQ29OSHB1b1BCMmxheDdLTGpaSW8yQnI2a2hmNXpHZFk2TlZ3Yw0KV2lqUXVrZHhBZzFXODQ3djVGM3ZxZiswYTdtdy96dXp4UE84b2grbCtvTlZGWnV6OFdRcE5GOGpYSmJqSjJtdw0KdDJlZENlRjhPdHE5aXVvZ0FwbTdHVkM5NnRGY2dZOHdjMzRCbHdJREFRQUJBb0lCQUVSRkFWODdadjNibDlLKw0KdzlIL1UxNHpzRWJ3alkxVForeHlNQURJbDRCTjVUckdVYWVTR2lHM1Njbmlqd25UR0NieGlwYkdGM0xYbjlZdg0KaVEwSGZCYmpHa0ZGWm41eDhOKzdhNHlXODU0anMyY3BiS2N3ZkszakpLbEM1TTdONFJiSm0rZTdrMDJJK3htWA0KYmJSY2tvY1FFQ2k5aXdhM0pPYTVEVVNxbm5WSjBqTGlFYkcyd2dMR1NMUE5XQjdNTG1tU2ZLb1dMMHFhUTFUbA0KQUdBM0ExU1JhQ21HbHpEcUZpNEhzQXFTOWcvY2FuKzlsbHMwLzlrUGJYbTgvTVI0TkV5NlNLTkQrVG45NXA0TA0KV29UN0RWUUhXUm5hSnNwT0RGZEdONi9EdlJDcTRPcjFBZ2dta2hSNUY2OGwveDZ1VmYrdVhIZWpsai9tdUs1Wg0KVlRWYmFzMENnWUVBM21vcElsV3VoMllSVGpLRVYvS3J6Uk5LNFdLODR3MktvYzdpbENxSFptM1g4Z29VY1lYWg0KQVdDUkxwbk5BNjM3bE40WFdBUGpzWUh2SVZiSDZqTkZJVXVVWjBvSDZKM0ptQWwyNnJ4RDF1MHlMV3ptVnpjZw0KYVQ3SlB2a3N6R01RajVQNnplbFpLeDNjRlhEYVpOUzQwOUdFTDJNRFhibVRYbElFbldXMEJJTUNnWUVBMU5tVA0KUVVBTUdwVVNXK3gvaE4zM1BvcVBvMWc3VVBiQzNXR0RzUk1TN1Axclg1cko1dWZGNHBPM0hNRWxCYnI3MUQvdQ0KZG5GRXpIU2R3amk0K3B3azZlN3ZxWmZZTjZxcm15ZE1XNStydFpMRDZGN0UwOHRsTlRjS1lxWEdFb01LU20rTQ0KZXh3NHpFaFR3eHJpb0k4OVNLMitwZjFsRlY2WStqT0ZBM2dEeWwwQ2dZQTl4T3dCbmh3bmZHQklBRjJpODBGbA0KWmZ0QTF5UXdScjErcks5ZWlPVXJ2RXZoNjR5NzdubFRHWmVZZ3B4ZWJqNVZuTXlNNUIralY3dXEzYWdOMGdsYg0KOHRxWFNRY2pRbGNQZzJsa1RiN2xmT0NYbnd2bG91aThjZlNBS2NHWVRkYjJtUmxwZ0dvRVFIbXo5ckNnbWpkdQ0KbEZ5S3NJdnB4clUzMTcxeVhTRitzd0tCZ0Nnci9FZlhYN0Z3cXl0Z1FsMEFTNWFwTGh6bEhTVFZ2Y2RzalI5Ug0KZTBQeFBGK3A1aXRIV1NpL09sTVovUFBNNjVTT2tHSVlWS1RGNmJScllqWWlYREV1b0ZzQXhwVVRDQnBiVzJUNw0KaUx2WFBGUlI5cElHbEU1cjJmRHVXcWpKeEpweDNUQ3JEZWs0U1Y3TVp6Tm9kV2VQS3lsRzN0b2VwWDZyVkpKbw0KQjBjZEFvR0JBSlBZY0lSdVVjRUE0T3NMTDYrQnZwbldVTFZQQUNPY3I3Z0dkOTg3bExJNm9DemQ0ajJWZ1ZTNQ0KQ1kvbkRXQ2lvZTFWbElab3ptQzFxdDF6dTg3ZE1WdS8vNGhRNklDYUQ1K2VIMnlQTVRuYmVmZVVESTRqVklFVg0KU21SSmo5YVVHWjRHWXd2TnhPbmtjeTI2eGs3RkpqbW12clJIK2FHRXNqZzVCWkxxUlAwdA0KLS0tLS1FTkQgUlNBIFBSSVZBVEUgS0VZLS0tLS0="', 'base64').toString('utf8');
+  async processRequest() {
+    const CLIENT_SDK_ID = "1f9edc97-c60c-40d7-becb-c1c6a2ec4963";
+    const PEM_KEY = Buffer.from(
+      '"LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQ0KTUlJRW93SUJBQUtDQVFFQXVPengvUE5IWUZjVDI5SEozWkk4eUVscVhGa3p6QzlQVnVXZkNVSmg1TDlzeml6TA0KSndadmNtWm9UdnJKd0JwdVRuUktOOGQ2cGVOdG1iVW5NVUVwOFArUCtTUFJ0Nm14NEt4S3c4NGZyaU9KVldRMw0KRnp0YzQzZDRPdDRWWnVsM2dLMDNpWjE5OWpIS09TY3pQR0lIOFVHbTBtdG5pQjY1RWNVbzBCcXliTkFydkZ4Mg0KbGY0NGdXZFlXa3pxN1JlWXJjV0IzUnQyZW9UQ29OSHB1b1BCMmxheDdLTGpaSW8yQnI2a2hmNXpHZFk2TlZ3Yw0KV2lqUXVrZHhBZzFXODQ3djVGM3ZxZiswYTdtdy96dXp4UE84b2grbCtvTlZGWnV6OFdRcE5GOGpYSmJqSjJtdw0KdDJlZENlRjhPdHE5aXVvZ0FwbTdHVkM5NnRGY2dZOHdjMzRCbHdJREFRQUJBb0lCQUVSRkFWODdadjNibDlLKw0KdzlIL1UxNHpzRWJ3alkxVForeHlNQURJbDRCTjVUckdVYWVTR2lHM1Njbmlqd25UR0NieGlwYkdGM0xYbjlZdg0KaVEwSGZCYmpHa0ZGWm41eDhOKzdhNHlXODU0anMyY3BiS2N3ZkszakpLbEM1TTdONFJiSm0rZTdrMDJJK3htWA0KYmJSY2tvY1FFQ2k5aXdhM0pPYTVEVVNxbm5WSjBqTGlFYkcyd2dMR1NMUE5XQjdNTG1tU2ZLb1dMMHFhUTFUbA0KQUdBM0ExU1JhQ21HbHpEcUZpNEhzQXFTOWcvY2FuKzlsbHMwLzlrUGJYbTgvTVI0TkV5NlNLTkQrVG45NXA0TA0KV29UN0RWUUhXUm5hSnNwT0RGZEdONi9EdlJDcTRPcjFBZ2dta2hSNUY2OGwveDZ1VmYrdVhIZWpsai9tdUs1Wg0KVlRWYmFzMENnWUVBM21vcElsV3VoMllSVGpLRVYvS3J6Uk5LNFdLODR3MktvYzdpbENxSFptM1g4Z29VY1lYWg0KQVdDUkxwbk5BNjM3bE40WFdBUGpzWUh2SVZiSDZqTkZJVXVVWjBvSDZKM0ptQWwyNnJ4RDF1MHlMV3ptVnpjZw0KYVQ3SlB2a3N6R01RajVQNnplbFpLeDNjRlhEYVpOUzQwOUdFTDJNRFhibVRYbElFbldXMEJJTUNnWUVBMU5tVA0KUVVBTUdwVVNXK3gvaE4zM1BvcVBvMWc3VVBiQzNXR0RzUk1TN1Axclg1cko1dWZGNHBPM0hNRWxCYnI3MUQvdQ0KZG5GRXpIU2R3amk0K3B3azZlN3ZxWmZZTjZxcm15ZE1XNStydFpMRDZGN0UwOHRsTlRjS1lxWEdFb01LU20rTQ0KZXh3NHpFaFR3eHJpb0k4OVNLMitwZjFsRlY2WStqT0ZBM2dEeWwwQ2dZQTl4T3dCbmh3bmZHQklBRjJpODBGbA0KWmZ0QTF5UXdScjErcks5ZWlPVXJ2RXZoNjR5NzdubFRHWmVZZ3B4ZWJqNVZuTXlNNUIralY3dXEzYWdOMGdsYg0KOHRxWFNRY2pRbGNQZzJsa1RiN2xmT0NYbnd2bG91aThjZlNBS2NHWVRkYjJtUmxwZ0dvRVFIbXo5ckNnbWpkdQ0KbEZ5S3NJdnB4clUzMTcxeVhTRitzd0tCZ0Nnci9FZlhYN0Z3cXl0Z1FsMEFTNWFwTGh6bEhTVFZ2Y2RzalI5Ug0KZTBQeFBGK3A1aXRIV1NpL09sTVovUFBNNjVTT2tHSVlWS1RGNmJScllqWWlYREV1b0ZzQXhwVVRDQnBiVzJUNw0KaUx2WFBGUlI5cElHbEU1cjJmRHVXcWpKeEpweDNUQ3JEZWs0U1Y3TVp6Tm9kV2VQS3lsRzN0b2VwWDZyVkpKbw0KQjBjZEFvR0JBSlBZY0lSdVVjRUE0T3NMTDYrQnZwbldVTFZQQUNPY3I3Z0dkOTg3bExJNm9DemQ0ajJWZ1ZTNQ0KQ1kvbkRXQ2lvZTFWbElab3ptQzFxdDF6dTg3ZE1WdS8vNGhRNklDYUQ1K2VIMnlQTVRuYmVmZVVESTRqVklFVg0KU21SSmo5YVVHWjRHWXd2TnhPbmtjeTI2eGs3RkpqbW12clJIK2FHRXNqZzVCWkxxUlAwdA0KLS0tLS1FTkQgUlNBIFBSSVZBVEUgS0VZLS0tLS0="',
+      "base64"
+    ).toString("utf8");
 
-		const sessionID = await this.yotiService.makeRequest2(CLIENT_SDK_ID, PEM_KEY);
+    const sessionID = await this.yotiService.createSession();
+    console.log("sessionID", sessionID);
+    const info = await this.yotiService.fetchSessionInfo(sessionID);
+    console.log("info", info);
+    const requirements = info.capture.required_resources
+      .filter((x: any) => x.type.includes("DOCUMENT"))
+      .map((resource: any) => {
+        if (resource.type === "ID_DOCUMENT") {
+          return {
+            requirement_id: resource.id,
+            document: {
+              type: resource.type,
+              country_code: resource.supported_countries[0].code,
+              document_type:
+                resource.supported_countries[0].supported_documents[0].type,
+            },
+          };
+        } else if (resource.type === "SUPPLEMENTARY_DOCUMENT") {
+          return {
+            requirement_id: resource.id,
+            document: {
+              type: resource.type,
+              country_code: resource.country_codes[0],
+              document_type: resource.document_types[0],
+            },
+          };
+        }
+      });
 
+    console.log("requirements", requirements);
+    const genInsturctions = await this.yotiService.generateInstructions(
+      sessionID,
+      requirements
+    );
+    console.log("genInsturctions", genInsturctions);
+    const fetchInstructions = await this.yotiService.fetchInstructionsPdf(
+      sessionID
+    );
+    console.log("fetchInstructions", fetchInstructions);
 
-		console.log('sessionID', sessionID);
-		return {
-			statusCode:200,
-			body: JSON.stringify({
-				access_token: sessionID
-			}),
-		};
-	}
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        access_token: sessionID,
+      }),
+    };
+  }
 }
--- src/services/YotiService.ts
+++ src/services/YotiService.ts
@@ -2,153 +2,255 @@
 import { Logger } from "@aws-lambda-powertools/logger";
 import crypto from "crypto";
 import * as jose from "node-jose";
-import {v4 as uuidv4} from 'uuid';
-import axios, { AxiosRequestConfig } from 'axios';
-
+import { v4 as uuidv4 } from "uuid";
+import axios, { AxiosRequestConfig } from "axios";
+import { HttpVerbsEnum } from "../utils/HttpVerbsEnum";
 
 export class YotiService {
   readonly logger: Logger;
 
   private static instance: YotiService;
 
-  constructor(logger: Logger) {
+  readonly CLIENT_SDK_ID: string;
+
+  readonly PEM_KEY: string;
+
+  constructor(logger: Logger, CLIENT_SDK_ID: string, PEM_KEY: string) {
     this.logger = logger;
+    this.CLIENT_SDK_ID = CLIENT_SDK_ID;
+    this.PEM_KEY = PEM_KEY;
   }
 
-  static getInstance(logger: Logger): YotiService {
+  static getInstance(
+    logger: Logger,
+    CLIENT_SDK_ID: string,
+    PEM_KEY: string
+  ): YotiService {
     if (!YotiService.instance) {
-      YotiService.instance = new YotiService(logger);
+      YotiService.instance = new YotiService(logger, CLIENT_SDK_ID, PEM_KEY);
     }
     return YotiService.instance;
   }
 
-	private getRSASignatureForMessage(message: string, PEM_KEY: string) {	
-		const sign = crypto.createSign("RSA-SHA256");
-		sign.update(message);
-		return sign.sign(PEM_KEY).toString("base64");
-	}
+  private getRSASignatureForMessage(message: string) {
+    const sign = crypto.createSign("RSA-SHA256");
+    sign.update(message);
+    return sign.sign(this.PEM_KEY).toString("base64");
+  }
 
-	public async makeRequest2(CLIENT_SDK_ID: string, PEM_KEY: string) {
-		const nonce = uuidv4();
-		const unixTimestamp = Date.now();
+  public async generateYotiRequest(Payload: {
+    method: any;
+    payloadJSON?: string;
+    endpoint: any;
+  }) {
+    const { method, endpoint } = Payload;
 
-		const queryString = `sdkId=${CLIENT_SDK_ID}&nonce=${nonce}&timestamp=${unixTimestamp}`;
+    const nonce = uuidv4();
+    const unixTimestamp = Date.now();
 
-		const endpointPath = `/sessions?${queryString}`
+    const queryString = `sdkId=${this.CLIENT_SDK_ID}&nonce=${nonce}&timestamp=${unixTimestamp}`;
 
+    const endpointPath = `${endpoint}?${queryString}`;
+
+		let base64String = ''
+
+    if ((method === HttpVerbsEnum.POST || method === HttpVerbsEnum.PUT) && Payload.payloadJSON) {
+      base64String = `&${Buffer.from(Payload.payloadJSON).toString(
+        "base64"
+      )}`;
+		}
+
+		const messageSignature = this.getRSASignatureForMessage(
+			`${method}&${endpointPath}${base64String}`
+		);
+			
+
+    const config: AxiosRequestConfig = {
+      headers: {
+        "X-Yoti-Auth-Digest": messageSignature,
+        "X-Yoti-SDK": "Node",
+        "X-Yoti-SDK-Version": "Node-4.1.0",
+        Accept: "application/json",
+      },
+    };
+
+    const response = {
+      url: `https://api.yoti.com/idverify/v1${endpointPath}`,
+      config,
+    };
+
+    return response;
+  }
+
+  public async createSession() {
+    const payloadJSON = {
+      session_deadline: "2023-05-05T23:59:59Z",
+      resources_ttl: "15780000",
+      ibv_options: {
+        support: "MANDATORY",
+      },
+      user_tracking_id: "some_id",
+      notifications: {
+        endpoint: "https://some-domain.example",
+        topics: ["SESSION_COMPLETION"],
+        auth_token: "string",
+        auth_type: "BASIC",
+      },
+      requested_checks: [
+        {
+          type: "IBV_VISUAL_REVIEW_CHECK",
+          config: {
+            manual_check: "IBV",
+          },
+        },
+        {
+          type: "PROFILE_DOCUMENT_MATCH",
+          config: {
+            manual_check: "IBV",
+          },
+        },
+        {
+          type: "DOCUMENT_SCHEME_VALIDITY_CHECK",
+          config: {
+            manual_check: "IBV",
+            scheme: "UK_DBS",
+          },
+        },
+        {
+          type: "ID_DOCUMENT_AUTHENTICITY",
+          config: {
+            manual_check: "FALLBACK",
+          },
+        },
+        {
+          type: "ID_DOCUMENT_FACE_MATCH",
+          config: {
+            manual_check: "FALLBACK",
+          },
+        },
+      ],
+      requested_tasks: [
+        {
+          type: "ID_DOCUMENT_TEXT_DATA_EXTRACTION",
+          config: {
+            manual_check: "FALLBACK",
+          },
+        },
+      ],
+      required_documents: [
+        {
+          type: "ID_DOCUMENT",
+          filter: {
+            type: "DOCUMENT_RESTRICTIONS",
+            inclusion: "WHITELIST",
+            documents: [
+              {
+                country_codes: ["GBR"],
+                document_types: ["PASSPORT"],
+              },
+            ],
+          },
+        },
+      ],
+      resources: {
+        applicant_profile: {
+          full_name: "John Doe",
+          date_of_birth: "1988-11-02",
+          name_prefix: "Mr",
+          structured_postal_address: {
+            address_format: 1,
+            building_number: "74",
+            address_line1: "AddressLine1",
+            town_city: "CityName",
+            postal_code: "E143RN",
+            country_iso: "GBR",
+            country: "United Kingdom",
+          },
+        },
+      },
+    };
+
+    const Payload = {
+      method: HttpVerbsEnum.POST,
+      payloadJSON: JSON.stringify(payloadJSON),
+      endpoint: "/sessions",
+    };
+
+    const yotiRequest = await this.generateYotiRequest(Payload);
+
+    console.log("yotiRequest", yotiRequest);
+
+    const { data } = await axios.post(
+      yotiRequest.url,
+      payloadJSON,
+      yotiRequest.config
+    );
+
+    return data.session_id;
+  }
+
+	public async fetchSessionInfo(sessionId: string) {
+
+		const Payload = {
+			method: HttpVerbsEnum.GET,
+			endpoint: `/sessions/${sessionId}/configuration`
+		 }
+
+    const yotiRequest = await this.generateYotiRequest(Payload)
+
+		const { data } = await axios.get(yotiRequest.url, yotiRequest.config)
+
+		console.log('fetchSessionInfo', data);
+
+		return data;
+  }
+
+	public async generateInstructions(sessionId: string, requirements: []) {
 		const payloadJSON = {
-			"session_deadline":"2023-05-05T23:59:59Z",
-			"resources_ttl":"15780000",
-			"ibv_options":{
-				 "support":"MANDATORY"
+			"contact_profile": {
+				 "first_name": "John",
+				 "last_name": "Doe",
+				 "email": "john.doe@gmail.com"
 			},
-			"user_tracking_id":"some_id",
-			"notifications":{
-				 "endpoint":"https://some-domain.example",
-				 "topics":[
-						"SESSION_COMPLETION"
-				 ],
-				 "auth_token":"string",
-				 "auth_type":"BASIC"
-			},
-			"requested_checks":[
-				 {
-						"type":"IBV_VISUAL_REVIEW_CHECK",
-						"config":{
-							 "manual_check":"IBV"
-						}
-				 },
-				 {
-						"type":"PROFILE_DOCUMENT_MATCH",
-						"config":{
-							 "manual_check":"IBV"
-						}
-				 },
-				 {
-						"type":"DOCUMENT_SCHEME_VALIDITY_CHECK",
-						"config":{
-							 "manual_check":"IBV",
-							 "scheme":"UK_DBS"
-						}
-				 },
-				 {
-						"type":"ID_DOCUMENT_AUTHENTICITY",
-						"config":{
-							 "manual_check":"FALLBACK"
-						}
-				 },
-				 {
-						"type":"ID_DOCUMENT_FACE_MATCH",
-						"config":{
-							 "manual_check":"FALLBACK"
-						}
-				 }
-			],
-			"requested_tasks":[
-				 {
-						"type":"ID_DOCUMENT_TEXT_DATA_EXTRACTION",
-						"config":{
-							 "manual_check":"FALLBACK"
-						}
-				 }
-			],
-			"required_documents":[
-				 {
-						"type":"ID_DOCUMENT",
-						"filter":{
-							 "type":"DOCUMENT_RESTRICTIONS",
-							 "inclusion":"WHITELIST",
-							 "documents":[
-									{
-										 "country_codes":[
-												"GBR"
-										 ],
-										 "document_types":[
-												"PASSPORT"
-										 ]
-									}
-							 ]
-						}
-				 }
-			],
-			"resources":{
-				 "applicant_profile":{
-						"full_name":"John Doe",
-						"date_of_birth":"1988-11-02",
-						"name_prefix":"Mr",
-						"structured_postal_address":{
-							 "address_format":1,
-							 "building_number":"74",
-							 "address_line1":"AddressLine1",
-							 "town_city":"CityName",
-							 "postal_code":"E143RN",
-							 "country_iso":"GBR",
-							 "country":"United Kingdom"
-						}
+			"documents": requirements,
+			"branch": {
+				 "type": "UK_POST_OFFICE",
+				 "name": "UK Post Office Branch",
+				 "address": "123 Post Office Road, London",
+				 "post_code": "ABC 123",
+				 "location": {
+						"latitude": 0.34322,
+						"longitude": -42.48372
 				 }
 			}
-	 }
-		const base64String = Buffer.from(JSON.stringify(payloadJSON)).toString('base64');
+	 };
+		const Payload = {
+			method: HttpVerbsEnum.PUT,
+			endpoint: `/sessions/${sessionId}/instructions`,
+			payloadJSON: JSON.stringify(payloadJSON),
+		 }
 
-		// Get message signature.
-		const messageSignature = this.getRSASignatureForMessage(
-			`POST&${endpointPath}&${base64String}`,
-			PEM_KEY
-		);
+    const yotiRequest = await this.generateYotiRequest(Payload)
 
-		const config: AxiosRequestConfig = {
-			headers: {
-				'X-Yoti-Auth-Digest': messageSignature,
-				'X-Yoti-SDK': 'Node',
-				'X-Yoti-SDK-Version': 'Node-4.1.0',
-				Accept: 'application/json',
-			},
-		};
+		const { data } = await axios.put(yotiRequest.url, payloadJSON, yotiRequest.config)
 
-		const { data, status } = await axios.post(`https://api.yoti.com/idverify/v1${endpointPath}`, payloadJSON, config)
+		console.log('generateInstructions', data);
 
-		return data.session_id;
-	}
+		return data;
+ 	}
 
+	 public async fetchInstructionsPdf(sessionId: string) {
+		const Payload = {
+			method: HttpVerbsEnum.GET,
+			endpoint: `/sessions/${sessionId}/instructions/pdf`
+		 }
 
+    const yotiRequest = await this.generateYotiRequest(Payload)
+
+		const { data } = await axios.get(yotiRequest.url, yotiRequest.config)
+
+		console.log('fetchInstructionsPdf', data);
+
+		return data;
+ 	}
 }
