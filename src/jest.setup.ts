27c123b6061e3b6be5eeff6b8a358e6f96f2ca42
Bhavana <bhavana.hemanth@digital.cabinet-office.gov.uk>
2023-04-05 12:11:19 +0100

    Fixing secrets post-commit error.

--- src/jest.setup.ts
+++ src/jest.setup.ts
@@ -6,7 +6,7 @@ process.env.GOVUKNOTIFY_TEMPLATE_ID = "bea2a584-4dca-4970-a90d-93977e752fdf"
 process.env.YOTISDK = "1f9edc97-c60c-40d7-becb-c1c6a2ec4963"
 process.env.YOTIBASEURL = "https://XXX-proxy.review-o.dev.account.gov.uk/yoti"
 process.env.YOTI_KEY_SSM_PATH = "/dev/YOTI/PRIVATEKEY"
-process.env.GOVUKNOTIFY_API_KEY_SSM_PATH = "/dev/f2f-gov-notify/GOVUKNOTIFY_API_KEY"
+process.env.GOVUKNOTIFY_API_KEY_SSM_PATH = "/dev/f2f-gov-notify/lsdkgl"
 
 
 
