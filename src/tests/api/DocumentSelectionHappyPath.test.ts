a2d32ef00f6f034bb06d7629bd3f7932a6f0a712
zmohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-04-21 15:40:14 +0100

    code cleanup

--- src/tests/api/DocumentSelectionHappyPath.test.ts
+++ src/tests/api/DocumentSelectionHappyPath.test.ts
@@ -4,7 +4,7 @@ import dataBrp from "../data/docSelectionPayloadBrpValid.json";
 import claimedIdentityTemplate from "../data/claimedIdentityTemplate.json";
 import { postDocumentSelection, sessionPost, stubStartPost, stubStartPostNoSharedClaims } from "../utils/ApiTestSteps";
 
-describe("E2E Happy Path /documentSelection Endpoint", () => {
+describe("Happy Path /documentSelection Endpoint", () => {
 	let sessionId: any;
 	beforeAll(async () => {
 		const stubResponse = await stubStartPost();
@@ -12,51 +12,27 @@ describe("E2E Happy Path /documentSelection Endpoint", () => {
 		sessionId = sessionResponse.data.session_id;
 	});
 
-	it("E2E Happy Path Journey - Passport", async () => {
+	it("Happy Path Journey - Passport", async () => {
 		expect(sessionId).toBeTruthy();
 		const response = await postDocumentSelection(dataPassport, sessionId);
 		expect(response.status).toBe(200);
 		expect(response.data).toBe("Instructions PDF Generated");
 	});
 
-	it("E2E Happy Path Journey - Drivers Licence", async () => {
+	it("Happy Path Journey - Drivers Licence", async () => {
 		expect(sessionId).toBeTruthy();
 		const response = await postDocumentSelection(dataDriversLicense, sessionId);
-		expect(response.status).toBe(200);
-		expect(response.data).toBe("Instructions PDF Generated");
-	});
-
-	it("E2E Happy Path Journey - Biometric Residence Permit", async () => {
-		expect(sessionId).toBeTruthy();
-		const response = await postDocumentSelection(dataBrp, sessionId);
 		expect(response.status).toBe(200);
 		expect(response.data).toBe("Instructions PDF Generated");
 	});
 });
 
-describe("E2E Happy Path /documentSelection Endpoint No Shared Claims", () => {
-	let sessionId: any;
-	beforeAll(async () => {
+describe("Happy Path /documentSelection Endpoint No Shared Claims", () => {
+
+	it("Happy Path Journey - Biometric Residence Permit - No Shared Claims", async () => {
 		const stubResponse = await stubStartPostNoSharedClaims(claimedIdentityTemplate);
 		const sessionResponse = await sessionPost(stubResponse.data.clientId, stubResponse.data.request);
-		sessionId = sessionResponse.data.session_id;
-	});
-
-	it("E2E Happy Path Journey - Passport - No Shared Claims", async () => {
-		expect(sessionId).toBeTruthy();
-		const response = await postDocumentSelection(dataPassport, sessionId);
-		expect(response.status).toBe(200);
-		expect(response.data).toBe("Instructions PDF Generated");
-	});
-
-	it("E2E Happy Path Journey - Drivers Licence - No Shared Claims", async () => {
-		expect(sessionId).toBeTruthy();
-		const response = await postDocumentSelection(dataDriversLicense, sessionId);
-		expect(response.status).toBe(200);
-		expect(response.data).toBe("Instructions PDF Generated");
-	});
-
-	it("E2E Happy Path Journey - Biometric Residence Permit - No Shared Claims", async () => {
+		const sessionId = sessionResponse.data.session_id;
 		expect(sessionId).toBeTruthy();
 		const response = await postDocumentSelection(dataBrp, sessionId);
 		expect(response.status).toBe(200);
