6df6145afc98327a4a70e787897769d007f2506e
zmohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-04-21 14:59:01 +0100

    code cleanup

--- src/tests/api/DocumentSelectionHappyPath.test.ts
+++ src/tests/api/DocumentSelectionHappyPath.test.ts
@@ -8,23 +8,26 @@ describe("E2E Happy Path /documentSelection Endpoint", () => {
 	let sessionId: any;
 	beforeAll(async () => {
 		const stubResponse = await stubStartPost();
-		const sessionResponse = await sessionPost(stubResponse.data.clientId, stubResponse.data.request)
+		const sessionResponse = await sessionPost(stubResponse.data.clientId, stubResponse.data.request);
 		sessionId = sessionResponse.data.session_id;
 	});
 
 	it("E2E Happy Path Journey - Passport", async () => {
+		expect(sessionId).toBeTruthy();
 		const response = await postDocumentSelection(dataPassport, sessionId);
 		expect(response.status).toBe(200);
 		expect(response.data).toBe("Instructions PDF Generated");
 	});
 
 	it("E2E Happy Path Journey - Drivers Licence", async () => {
+		expect(sessionId).toBeTruthy();
 		const response = await postDocumentSelection(dataDriversLicense, sessionId);
 		expect(response.status).toBe(200);
 		expect(response.data).toBe("Instructions PDF Generated");
 	});
 
 	it("E2E Happy Path Journey - Biometric Residence Permit", async () => {
+		expect(sessionId).toBeTruthy();
 		const response = await postDocumentSelection(dataBrp, sessionId);
 		expect(response.status).toBe(200);
 		expect(response.data).toBe("Instructions PDF Generated");
@@ -35,23 +38,26 @@ describe("E2E Happy Path /documentSelection Endpoint No Shared Claims", () => {
 	let sessionId: any;
 	beforeAll(async () => {
 		const stubResponse = await stubStartPostNoSharedClaims(claimedIdentityTemplate);
-		const sessionResponse = await sessionPost(stubResponse.data.clientId, stubResponse.data.request)
+		const sessionResponse = await sessionPost(stubResponse.data.clientId, stubResponse.data.request);
 		sessionId = sessionResponse.data.session_id;
 	});
 
 	it("E2E Happy Path Journey - Passport - No Shared Claims", async () => {
+		expect(sessionId).toBeTruthy();
 		const response = await postDocumentSelection(dataPassport, sessionId);
 		expect(response.status).toBe(200);
 		expect(response.data).toBe("Instructions PDF Generated");
 	});
 
-	it("E2E Happy Path Journey - Drivers Licence", async () => {
+	it("E2E Happy Path Journey - Drivers Licence - No Shared Claims", async () => {
+		expect(sessionId).toBeTruthy();
 		const response = await postDocumentSelection(dataDriversLicense, sessionId);
 		expect(response.status).toBe(200);
 		expect(response.data).toBe("Instructions PDF Generated");
 	});
 
-	it("E2E Happy Path Journey - Biometric Residence Permit", async () => {
+	it("E2E Happy Path Journey - Biometric Residence Permit - No Shared Claims", async () => {
+		expect(sessionId).toBeTruthy();
 		const response = await postDocumentSelection(dataBrp, sessionId);
 		expect(response.status).toBe(200);
 		expect(response.data).toBe("Instructions PDF Generated");
--- src/tests/api/DocumentSelectionNegativePath.test.ts
+++ src/tests/api/DocumentSelectionNegativePath.test.ts
@@ -6,12 +6,11 @@ describe("E2E Negative Path /documentSelection Endpoint", () => {
 	let sessionId: any;
 	beforeAll(async () => {
 		const stubResponse = await stubStartPost();
-		const sessionResponse = await sessionPost(stubResponse.data.clientId, stubResponse.data.request)
+		const sessionResponse = await sessionPost(stubResponse.data.clientId, stubResponse.data.request);
 		sessionId = sessionResponse.data.session_id;
 	});
 
 	it("E2E Negative Path Journey - Invalid Request Payload", async () => {
-		console.log(sessionId);
 		const response = await postDocumentSelection(dataDriversLicense, sessionId);
 		expect(response.status).toBe(400);
 		expect(response.data).toEqual({ "message": "Invalid request body" });
@@ -19,8 +18,6 @@ describe("E2E Negative Path /documentSelection Endpoint", () => {
 	});
 
 	it("E2E Happy Path Journey - Incorrect Session Id", async () => {
-		console.log(sessionId);
-
 		const response = await postDocumentSelection(dataPassport, "sessionId");
 		expect(response.status).toBe(400);
 		expect(response.data).toBe("Session id must be a valid uuid");
