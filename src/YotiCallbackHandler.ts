880a205e4e5b9692d59d326e0490ca4431e89083
Viveak Vadivelkarasan <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-04-28 14:51:46 +0100

    F2F | BE | YOTI Integration | Build Yoti Callback Handler (#62)

---
+++ src/YotiCallbackHandler.ts
@@ -0,0 +1,93 @@
+import { SQSEvent, SQSRecord } from "aws-lambda";
+import { Logger } from "@aws-lambda-powertools/logger";
+import { Metrics } from "@aws-lambda-powertools/metrics";
+import { Response } from "./utils/Response";
+
+import { LambdaInterface } from "@aws-lambda-powertools/commons";
+
+import { Constants } from "./utils/Constants";
+
+import { BatchItemFailure } from "./utils/BatchItemFailure";
+import { HttpCodesEnum } from "./models/enums/HttpCodesEnum";
+import { getParameter } from "./utils/Config";
+import { EnvironmentVariables } from "./services/EnvironmentVariables";
+import { YotiCallbackProcessor } from "./services/YotiCallbackProcessor";
+import { ServicesEnum } from "./models/enums/ServicesEnum";
+
+const POWERTOOLS_METRICS_NAMESPACE = process.env.POWERTOOLS_METRICS_NAMESPACE ? process.env.POWERTOOLS_METRICS_NAMESPACE : Constants.F2F_METRICS_NAMESPACE;
+const POWERTOOLS_LOG_LEVEL = process.env.POWERTOOLS_LOG_LEVEL ? process.env.POWERTOOLS_LOG_LEVEL : Constants.DEBUG;
+const POWERTOOLS_SERVICE_NAME = process.env.POWERTOOLS_SERVICE_NAME ? process.env.POWERTOOLS_SERVICE_NAME : Constants.YOTI_CALLBACK_SVC_NAME;
+
+const logger = new Logger({
+	logLevel: POWERTOOLS_LOG_LEVEL,
+	serviceName: POWERTOOLS_SERVICE_NAME,
+});
+
+const metrics = new Metrics({ namespace: POWERTOOLS_METRICS_NAMESPACE, serviceName: POWERTOOLS_SERVICE_NAME });
+
+let YOTI_PRIVATE_KEY: string;
+
+class YotiCallbackHandler implements LambdaInterface {
+	private readonly environmentVariables = new EnvironmentVariables(logger, ServicesEnum.CALLBACK_SERVICE);
+
+	@metrics.logMetrics({ throwOnEmptyMetrics: false, captureColdStartMetric: true })
+	async handler(event: SQSEvent, context: any): Promise<any> {
+		if (event.Records.length === 1) {
+			const record: SQSRecord = event.Records[0];
+			logger.debug("Starting to process record", { record });
+			const batchFailures: BatchItemFailure[] = [];
+
+			try {
+				const body = JSON.parse(record.body);
+				logger.debug("Parsed SQS event body", body);
+				if (!YOTI_PRIVATE_KEY) {
+					logger.info({ message: "Fetching YOTI_PRIVATE_KEY from SSM" });
+					try {
+						YOTI_PRIVATE_KEY = await getParameter(this.environmentVariables.yotiKeySsmPath());
+					} catch (err) {
+						logger.error(`failed to get param from ssm at ${this.environmentVariables.yotiKeySsmPath()}`, { err });
+						throw err;
+					}
+				}
+				
+				await YotiCallbackProcessor.getInstance(logger, metrics, YOTI_PRIVATE_KEY).processRequest(body);
+
+				logger.debug("Finished processing record from SQS");
+				return new Response(HttpCodesEnum.OK, {
+					batchItemFailures: [],
+				});
+
+			} catch (error: any) {
+				if (error.obj?.shouldThrow) {
+					logger.error("Error encountered", { error });
+					error.obj = undefined;
+					batchFailures.push(new BatchItemFailure(record.messageId));
+					const sqsBatchResponse = { batchItemFailures: batchFailures };
+					logger.error("Returning batch item failure so it can be retried", { sqsBatchResponse });
+					return sqsBatchResponse;
+				} else {
+					const appErrorCode = error.appCode;
+					const statusCode = error.statusCode ? error.statusCode : HttpCodesEnum.SERVER_ERROR;
+					const body = {
+						statusCode,
+						message: error.message || JSON.stringify(error),
+						batchItemFailures: [],
+						error,
+						appErrorCode,
+					};
+
+					logger.error({ message: "VC could not be sent. Returning failed message ", error });
+					return new Response(statusCode, body, appErrorCode);
+				}
+			}
+
+		} else {
+			logger.warn("Unexpected no of records received");
+			return new Response(HttpCodesEnum.BAD_REQUEST, "Unexpected no of records received");
+		}
+	}
+
+}
+
+const handlerClass = new YotiCallbackHandler();
+export const lambdaHandler = handlerClass.handler.bind(handlerClass);
---
+++ src/tests/unit/services/YotiCallbackProcessor.test.ts
@@ -0,0 +1,575 @@
+import { Metrics } from "@aws-lambda-powertools/metrics";
+import { mock } from "jest-mock-extended";
+import { Logger } from "@aws-lambda-powertools/logger";
+import { F2fService } from "../../../services/F2fService";
+import { Response } from "../../../utils/Response";
+import { HttpCodesEnum } from "../../../utils/HttpCodesEnum";
+import { YotiCallbackProcessor } from "../../../services/YotiCallbackProcessor";
+import { YotiService } from "../../../services/YotiService";
+import { PersonIdentityItem } from "../../../models/PersonIdentityItem";
+import { YotiCompletedSession } from "../../../models/YotiPayloads";
+import { ISessionItem } from "../../../models/ISessionItem";
+import { AuthSessionState } from "../../../models/enums/AuthSessionState";
+import { MockKmsJwtAdapterForVc } from "../utils/MockJwtVerifierSigner";
+import { absoluteTimeNow } from "../../../utils/DateTimeUtils";
+import { Constants } from "../../../utils/Constants";
+
+let mockYotiCallbackProcessor: YotiCallbackProcessor;
+const mockF2fService = mock<F2fService>();
+const mockYotiService = mock<YotiService>();
+
+const logger = mock<Logger>();
+const metrics = new Metrics({ namespace: "F2F" });
+
+jest.mock("../../../utils/KmsJwtAdapter");
+const passingKmsJwtAdapterFactory = (_signingKeys: string) => new MockKmsJwtAdapterForVc(true);
+
+function getMockSessionItem(): ISessionItem {
+	const sessionInfo: ISessionItem = {
+		sessionId: "RandomF2FSessionID",
+		clientId: "ipv-core-stub",
+		// pragma: allowlist nextline secret
+		accessToken: "AbCdEf123456",
+		clientSessionId: "sdfssg",
+		authorizationCode: "",
+		authorizationCodeExpiryDate: 0,
+		redirectUri: "http://localhost:8085/callback",
+		accessTokenExpiryDate: 0,
+		expiryDate: 221848913376,
+		createdDate: 1675443004,
+		state: "Y@atr",
+		subject: "sub",
+		persistentSessionId: "sdgsdg",
+		clientIpAddress: "127.0.0.1",
+		attemptCount: 1,
+		authSessionState: AuthSessionState.F2F_DATA_RECEIVED,
+	};
+	return sessionInfo;
+}
+
+function getPersonIdentityItem(): PersonIdentityItem {
+	const personIdentityItem: PersonIdentityItem = {
+		"addresses": [
+			{
+				"addressCountry": "United Kingdom",
+				"buildingName": "Sherman",
+				"uprn": 123456789,
+				"streetName": "Wallaby Way",
+				"postalCode": "F1 1SH",
+				"buildingNumber": "32",
+				"addressLocality": "Sidney",
+			},
+		],
+		"sessionId": "RandomF2FSessionID",
+		"emailAddress": "test123@gov.uk",
+		"birthDate": [
+			{
+				"value":"1960-02-02",
+			},
+		],
+		"name": [
+			{
+				"nameParts": [
+					{
+						"type": "GivenName",
+						"value": "Frederick",
+					},
+					{
+						"type": "GivenName",
+						"value": "Joseph",
+					},
+					{
+						"type": "FamilyName",
+						"value": "Flintstone",
+					},
+				],
+			},
+		],
+	};
+	return personIdentityItem;
+}
+
+function getCompletedYotiSession(): YotiCompletedSession {
+	const completedYotiSession: YotiCompletedSession = {
+		"client_session_token_ttl": 2637158,
+		"session_id": "f4340e05-03ec-48fe-bf6b-5946089bb4f3",
+		"state": "COMPLETED",
+		"resources": {
+			"id_documents": [
+				{
+					"id": "355e9f80-6f2a-470c-a72e-e13c7417fb9a",
+					"tasks": [
+						{
+							"type": "ID_DOCUMENT_TEXT_DATA_EXTRACTION",
+							"id": "a9521ad3-8b17-4115-b75f-0f3de86a4e2f",
+							"state": "DONE",
+							"created": "2023-04-05T10:11:03Z",
+							"last_updated": "2023-04-05T10:14:02Z",
+							"generated_checks": [],
+							"generated_media": [
+								{
+									"id": "6b9efa9d-4968-4f5d-8536-377008526b26",
+									"type": "JSON",
+								},
+							],
+						},
+					],
+					"source": {
+						"type": "IBV",
+					},
+					"created_at": "2023-04-05T10:11:03Z",
+					"last_updated": "2023-04-05T10:14:02Z",
+					"document_type": "PASSPORT",
+					"issuing_country": "GBR",
+					"pages": [
+						{
+							"capture_method": "CAMERA",
+							"media": {
+								"id": "cbb1503e-0fd5-456c-a023-2cfda11697e1",
+								"type": "IMAGE",
+								"created": "2023-04-05T10:13:50Z",
+								"last_updated": "2023-04-05T10:13:50Z",
+							},
+							"frames": [
+								{
+									"media": {
+										"id": "5e7cee77-3718-4a99-a8b8-eb2f8969449a",
+										"type": "IMAGE",
+										"created": "2023-04-05T10:13:52Z",
+										"last_updated": "2023-04-05T10:13:52Z",
+									},
+								},
+								{
+									"media": {
+										"id": "82f2b1db-9bef-4a0a-ad6c-622da80b0894",
+										"type": "IMAGE",
+										"created": "2023-04-05T10:13:54Z",
+										"last_updated": "2023-04-05T10:13:54Z",
+									},
+								},
+								{
+									"media": {
+										"id": "df322a12-ce9b-4790-bf96-810b84982129",
+										"type": "IMAGE",
+										"created": "2023-04-05T10:13:56Z",
+										"last_updated": "2023-04-05T10:13:56Z",
+									},
+								},
+							],
+						},
+					],
+					"document_fields": {
+						"media": {
+							"id": "6b9efa9d-4968-4f5d-8536-377008526b26",
+							"type": "JSON",
+							"created": "2023-04-05T10:14:02Z",
+							"last_updated": "2023-04-05T10:14:02Z",
+						},
+					},
+					"document_id_photo": {
+						"media": {
+							"id": "2c4b59e8-fb68-4c7c-ac35-edac5cafa687",
+							"type": "IMAGE",
+							"created": "2023-04-05T10:14:02Z",
+							"last_updated": "2023-04-05T10:14:02Z",
+						},
+					},
+				},
+			],
+			"supplementary_documents": [],
+			"liveness_capture": [],
+			"face_capture": [
+				{
+					"id": "3c3ea0ac-2902-4c7c-a0ba-cf5daef5cc38",
+					"tasks": [],
+					"source": {
+						"type": "IBV",
+					},
+					"created_at": "2023-04-05T10:17:58Z",
+					"last_updated": "2023-04-05T10:18:06Z",
+					"image": {
+						"media": {
+							"id": "5b77cd1f-a15a-4748-ba5c-92913be6974d",
+							"type": "IMAGE",
+							"created": "2023-04-05T10:18:06Z",
+							"last_updated": "2023-04-05T10:18:06Z",
+						},
+					},
+				},
+			],
+			"applicant_profiles": [
+				{
+					"id": "88eb9e0b-acd8-4f49-940b-89740d649508",
+					"tasks": [],
+					"source": {
+						"type": "RELYING_BUSINESS",
+					},
+					"created_at": "2023-04-05T08:13:56Z",
+					"last_updated": "2023-04-05T08:13:56Z",
+					"media": {
+						"id": "119b8fb5-626d-43ad-9a09-450c1580d9e1",
+						"type": "JSON",
+						"created": "2023-04-05T08:13:56Z",
+						"last_updated": "2023-04-05T08:13:56Z",
+					},
+				},
+			],
+		},
+		"checks": [
+			{
+				"type": "ID_DOCUMENT_AUTHENTICITY",
+				"id": "0a3f07a4-def6-4d62-bc99-9893459c1bfb",
+				"state": "DONE",
+				"resources_used": [
+					"355e9f80-6f2a-470c-a72e-e13c7417fb9a",
+				],
+				"generated_media": [],
+				"report": {
+					"recommendation": {
+						"value": "APPROVE",
+					},
+					"breakdown": [
+						{
+							"sub_check": "document_in_date",
+							"result": "PASS",
+							"details": [],
+						},
+						{
+							"sub_check": "fraud_list_check",
+							"result": "PASS",
+							"details": [],
+						},
+						{
+							"sub_check": "hologram",
+							"result": "PASS",
+							"details": [],
+						},
+						{
+							"sub_check": "hologram_movement",
+							"result": "PASS",
+							"details": [],
+						},
+						{
+							"sub_check": "mrz_validation",
+							"result": "PASS",
+							"details": [],
+						},
+						{
+							"sub_check": "no_sign_of_forgery",
+							"result": "PASS",
+							"details": [],
+						},
+						{
+							"sub_check": "no_sign_of_tampering",
+							"result": "PASS",
+							"details": [],
+						},
+						{
+							"sub_check": "ocr_mrz_comparison",
+							"result": "PASS",
+							"details": [],
+						},
+						{
+							"sub_check": "other_security_features",
+							"result": "PASS",
+							"details": [],
+						},
+						{
+							"sub_check": "physical_document_captured",
+							"result": "PASS",
+							"details": [],
+						},
+						{
+							"sub_check": "yoti_fraud_list_check",
+							"result": "FAIL",
+							"details": [
+								{
+									"name": "provider_org",
+									"value": "Yoti Ltd",
+								},
+							],
+						},
+					],
+				},
+				"created": "2023-04-05T10:18:16Z",
+				"last_updated": "2023-04-05T10:18:38Z",
+			},
+			{
+				"type": "ID_DOCUMENT_FACE_MATCH",
+				"id": "b8020135-612a-431b-a457-76d4a9959918",
+				"state": "DONE",
+				"resources_used": [
+					"355e9f80-6f2a-470c-a72e-e13c7417fb9a",
+					"3c3ea0ac-2902-4c7c-a0ba-cf5daef5cc38",
+				],
+				"generated_media": [],
+				"report": {
+					"recommendation": {
+						"value": "APPROVE",
+					},
+					"breakdown": [
+						{
+							"sub_check": "ai_face_match",
+							"result": "PASS",
+							"details": [
+								{
+									"name": "confidence_score",
+									"value": "0.99",
+								},
+							],
+						},
+						{
+							"sub_check": "manual_face_match",
+							"result": "PASS",
+							"details": [],
+						},
+						{
+							"sub_check": "yoti_fraud_list_check",
+							"result": "FAIL",
+							"details": [
+								{
+									"name": "provider_org",
+									"value": "Yoti Ltd",
+								},
+							],
+						},
+					],
+				},
+				"created": "2023-04-05T10:18:16Z",
+				"last_updated": "2023-04-05T10:37:19Z",
+			},
+			{
+				"type": "IBV_VISUAL_REVIEW_CHECK",
+				"id": "d3304228-a7f3-4b33-92aa-569209de8a0a",
+				"state": "DONE",
+				"resources_used": [
+					"355e9f80-6f2a-470c-a72e-e13c7417fb9a",
+				],
+				"generated_media": [],
+				"report": {
+					"recommendation": {
+						"value": "APPROVE",
+					},
+					"breakdown": [],
+				},
+				"created": "2023-04-05T10:18:16Z",
+				"last_updated": "2023-04-05T10:18:16Z",
+			},
+			{
+				"type": "DOCUMENT_SCHEME_VALIDITY_CHECK",
+				"id": "f19eba50-0cf6-434a-8592-9f0d97141495",
+				"state": "DONE",
+				"resources_used": [
+					"355e9f80-6f2a-470c-a72e-e13c7417fb9a",
+				],
+				"generated_media": [],
+				"report": {
+					"recommendation": {
+						"value": "APPROVE",
+					},
+					"breakdown": [],
+				},
+				"created": "2023-04-05T10:18:16Z",
+				"last_updated": "2023-04-05T10:18:16Z",
+				"scheme": "UK_DBS",
+			},
+			{
+				"type": "PROFILE_DOCUMENT_MATCH",
+				"id": "424a95ff-7ef6-4db4-b721-cb94fb4aed70",
+				"state": "DONE",
+				"resources_used": [
+					"355e9f80-6f2a-470c-a72e-e13c7417fb9a",
+					"88eb9e0b-acd8-4f49-940b-89740d649508",
+				],
+				"generated_media": [],
+				"report": {
+					"recommendation": {
+						"value": "APPROVE",
+					},
+					"breakdown": [],
+				},
+				"created": "2023-04-05T10:18:16Z",
+				"last_updated": "2023-04-05T10:18:16Z",
+			},
+		],
+		"user_tracking_id": "some_id",
+	};
+	return completedYotiSession;
+}
+
+const VALID_REQUEST = {
+	"session_id":"b988e9c8-47c6-430c-9ca3-8cdacd85ee91",
+	"topic" : "session_completion",
+};
+
+
+describe("YotiCallbackProcessor", () => {
+	let personIdentityItem: PersonIdentityItem, f2fSessionItem: ISessionItem, completedYotiSession: YotiCompletedSession;
+	beforeAll(() => {
+		mockYotiCallbackProcessor = new YotiCallbackProcessor(logger, metrics, "YOTIPRIM");
+		// @ts-ignore
+		mockYotiCallbackProcessor.f2fService = mockF2fService;
+		// @ts-ignore
+		mockYotiCallbackProcessor.yotiService = mockYotiService;
+
+		personIdentityItem = getPersonIdentityItem();
+		completedYotiSession = getCompletedYotiSession();
+		f2fSessionItem = getMockSessionItem();
+
+		
+	});
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+		// @ts-ignore
+		mockYotiCallbackProcessor.kmsJwtAdapter = passingKmsJwtAdapterFactory();
+	});
+
+	it("Return successful response with 200 OK when YOTI session created", async () => {
+		mockYotiService.getCompletedSessionInfo.mockResolvedValueOnce(completedYotiSession);
+		mockF2fService.getSessionByYotiId.mockResolvedValueOnce(f2fSessionItem);
+		mockF2fService.getPersonIdentityById.mockResolvedValueOnce(personIdentityItem);
+
+		// @ts-ignore
+		mockYotiCallbackProcessor.verifiableCredentialService.kmsJwtAdapter = passingKmsJwtAdapterFactory();
+
+		const out: Response = await mockYotiCallbackProcessor.processRequest(VALID_REQUEST);
+
+		
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		expect(mockF2fService.sendToTXMA).toHaveBeenCalledTimes(2);
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		expect(mockF2fService.sendToTXMA).toHaveBeenNthCalledWith(1, { "client_id": "ipv-core-stub", "component_id": "https://XXX-c.env.account.gov.uk", "event_name": "F2F_YOTI_END", "timestamp": absoluteTimeNow(), "user": { "govuk_signin_journey_id": "sdfssg", "ip_address": "127.0.0.1", "persistent_session_id": "sdgsdg", "session_id": "RandomF2FSessionID", "transaction_id": "", "user_id": "ipv-core-stub" } });
+		expect(mockF2fService.sendToTXMA).toHaveBeenNthCalledWith(2, { "client_id": "ipv-core-stub", "component_id": "https://XXX-c.env.account.gov.uk", "event_name": "F2F_CRI_VC_ISSUED", "timestamp": absoluteTimeNow(), "user": { "govuk_signin_journey_id": "sdfssg", "ip_address": "127.0.0.1", "persistent_session_id": "sdgsdg", "session_id": "RandomF2FSessionID", "transaction_id": "", "user_id": "ipv-core-stub" } });
+
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		expect(mockF2fService.sendToIPVCore).toHaveBeenCalledTimes(1);
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		expect(mockF2fService.sendToIPVCore).toHaveBeenCalledWith({
+			sub: "sub",
+			state: "Y@atr",
+			"https://vocab.account.gov.uk/v1/credentialJWT": JSON.stringify({
+				"sub":"sub",
+				"nbf":absoluteTimeNow(),
+				"iss":"https://XXX-c.env.account.gov.uk",
+				"iat":absoluteTimeNow(),
+				"vc":{
+					 "@context":[
+						Constants.W3_BASE_CONTEXT,
+						Constants.DI_CONTEXT,
+					 ],
+					 "type": [Constants.VERIFIABLE_CREDENTIAL, Constants.IDENTITY_CHECK_CREDENTIAL],
+					 "credentialSubject":{
+						"name":[
+								 {
+								"nameParts":[
+											 {
+										"type":"GivenName",
+										"value":"Frederick",
+											 },
+											 {
+										"type":"GivenName",
+										"value":"Joseph",
+											 },
+											 {
+										"type":"FamilyName",
+										"value":"Flintstone",
+											 },
+								],
+								 },
+						],
+						"birthDate":[
+								 {
+								"value":"1960-02-02",
+								 },
+						],
+					 },
+				},
+			}),
+		});
+		expect(out.statusCode).toBe(HttpCodesEnum.OK);
+		expect(out.body).toBe("OK");
+	});
+
+	it("Throw sever error if completed Yoti session can not be found", async () => {
+		mockYotiService.getCompletedSessionInfo.mockResolvedValueOnce(undefined);
+
+		return expect(mockYotiCallbackProcessor.processRequest(VALID_REQUEST)).rejects.toThrow(expect.objectContaining({
+			statusCode: HttpCodesEnum.SERVER_ERROR,
+			message: "Yoti Session not found",
+		}));
+	});
+
+	it("Throw sever error if F2F Session can not be found", async () => {
+		mockYotiService.getCompletedSessionInfo.mockResolvedValueOnce(completedYotiSession);
+		mockF2fService.getSessionByYotiId.mockResolvedValueOnce(undefined);
+
+		return expect(mockYotiCallbackProcessor.processRequest(VALID_REQUEST)).rejects.toThrow(expect.objectContaining({
+			statusCode: HttpCodesEnum.SERVER_ERROR,
+			message: "Missing Info in Session Table",
+		}));
+	});
+
+	it("Throw sever error if F2F Session can not be found", async () => {
+		mockYotiService.getCompletedSessionInfo.mockResolvedValueOnce(completedYotiSession);
+		mockF2fService.getSessionByYotiId.mockResolvedValueOnce(f2fSessionItem);
+		mockF2fService.getPersonIdentityById.mockResolvedValueOnce(undefined);
+
+		return expect(mockYotiCallbackProcessor.processRequest(VALID_REQUEST)).rejects.toThrow(expect.objectContaining({
+			statusCode: HttpCodesEnum.SERVER_ERROR,
+			message: "Missing Info in Person Identity Table",
+		}));
+	});
+
+	it("Return 200 when write to txMA fails", async () => {
+		mockYotiService.getCompletedSessionInfo.mockResolvedValueOnce(completedYotiSession);
+		mockF2fService.getSessionByYotiId.mockResolvedValueOnce(f2fSessionItem);
+		mockF2fService.getPersonIdentityById.mockResolvedValueOnce(personIdentityItem);
+		mockF2fService.sendToTXMA.mockRejectedValue({});
+
+		// @ts-ignore
+		mockYotiCallbackProcessor.verifiableCredentialService.kmsJwtAdapter = passingKmsJwtAdapterFactory();
+
+		const out: Response = await mockYotiCallbackProcessor.processRequest(VALID_REQUEST);
+
+		
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		expect(mockF2fService.sendToTXMA).toHaveBeenCalledTimes(2);
+
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		expect(logger.error).toHaveBeenNthCalledWith(1, "Failed to write TXMA event F2F_YOTI_END to SQS queue.");
+		expect(logger.error).toHaveBeenNthCalledWith(2, "Failed to write TXMA event F2F_CRI_VC_ISSUED to SQS queue.");
+		expect(out.statusCode).toBe(HttpCodesEnum.OK);
+	});
+
+
+	it("Throws server error if failure to send to IPVCore queue", async () => {
+		mockYotiService.getCompletedSessionInfo.mockResolvedValueOnce(completedYotiSession);
+		mockF2fService.getSessionByYotiId.mockResolvedValueOnce(f2fSessionItem);
+		mockF2fService.getPersonIdentityById.mockResolvedValueOnce(personIdentityItem);
+
+		// @ts-ignore
+		mockYotiCallbackProcessor.verifiableCredentialService.kmsJwtAdapter = passingKmsJwtAdapterFactory();
+
+		mockF2fService.sendToIPVCore.mockRejectedValueOnce("Failed to send to IPV Core");
+
+		return expect(mockYotiCallbackProcessor.processRequest(VALID_REQUEST)).rejects.toThrow(expect.objectContaining({
+			statusCode: HttpCodesEnum.SERVER_ERROR,
+			message: "Failed to send to IPV Core",
+		}));
+	});
+
+	it("Throws server error if session in Yoti is not completed", async () => {
+		completedYotiSession.state = "ONGOING";
+		mockYotiService.getCompletedSessionInfo.mockResolvedValueOnce(completedYotiSession);
+
+		return expect(mockYotiCallbackProcessor.processRequest(VALID_REQUEST)).rejects.toThrow(expect.objectContaining({
+			statusCode: HttpCodesEnum.SERVER_ERROR,
+			message: "Yoti Session not complete",
+		}));
+	});
+});
