fda833656d14a2bdac92b7fdfac095131c72202b
zmohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-06-02 13:21:20 +0100

    /session endpoint updates and f2f api test updates

--- src/tests/api/DocumentSelectionNegativePath.test.ts
+++ src/tests/api/DocumentSelectionNegativePath.test.ts
@@ -1,16 +1,19 @@
 import * as dataDriversLicense from "../data/docSelectionPayloadDriversLicenceInvalid.json";
 import * as dataPassport from "../data/docSelectionPayloadPassportValid.json";
 import { postDocumentSelection, startStubServiceAndReturnSessionId } from "../utils/ApiTestSteps";
+import f2fStubPayload from "../data/exampleStubPayload.json";
+
 
 describe.skip("E2E Negative Path /documentSelection Endpoint", () => {
 	let sessionId: any;
 	beforeEach(async () => {
-		const sessionResponse = await startStubServiceAndReturnSessionId();
+		f2fStubPayload.yotiMockID = "0000";
+		const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
 		sessionId = sessionResponse.data.session_id;
+		console.log("session id: " + sessionId);
 	});
 
 	it("E2E Negative Path Journey - Invalid Request Payload", async () => {
-		console.log(sessionId);
 		const response = await postDocumentSelection(dataDriversLicense, sessionId);
 		expect(response.status).toBe(400);
 		expect(response.data).toEqual({ "message": "Invalid request body" });
