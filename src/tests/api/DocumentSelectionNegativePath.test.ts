a42c5e18cc8df78c8aa256e5a759ddfd263137de
zmohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-06-02 13:56:50 +0100

    /session endpoint updates and f2f api test updates

--- src/tests/api/DocumentSelectionNegativePath.test.ts
+++ src/tests/api/DocumentSelectionNegativePath.test.ts
@@ -5,28 +5,28 @@ import f2fStubPayload from "../data/exampleStubPayload.json";
 import { constants } from "../utils/ApiConstants";
 
 
-// describe("E2E Negative Path /documentSelection Endpoint", () => {
-// 	let sessionId: any;
-// 	beforeEach(async () => {
-// 		f2fStubPayload.yotiMockID = "0000";
-// 		const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
-// 		sessionId = sessionResponse.data.session_id;
-// 		console.log("session id: " + sessionId);
-// 	});
+describe("E2E Negative Path /documentSelection Endpoint", () => {
+	let sessionId: any;
+	beforeEach(async () => {
+		f2fStubPayload.yotiMockID = "0000";
+		const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
+		sessionId = sessionResponse.data.session_id;
+		console.log("session id: " + sessionId);
+	});
 
-// 	it("E2E Negative Path Journey - Invalid Request Payload", async () => {
-// 		const response = await postDocumentSelection(dataDriversLicense, sessionId);
-// 		expect(response.status).toBe(400);
-// 		expect(response.data).toEqual({ "message": "Invalid request body" });
+	it("E2E Negative Path Journey - Invalid Request Payload", async () => {
+		const response = await postDocumentSelection(dataDriversLicense, sessionId);
+		expect(response.status).toBe(400);
+		expect(response.data).toEqual({ "message": "Invalid request body" });
 
-// 	});
+	});
 
-// 	it("E2E Happy Path Journey - Incorrect Session Id", async () => {
-// 		const response = await postDocumentSelection(dataPassport, "sessionId");
-// 		expect(response.status).toBe(400);
-// 		expect(response.data).toEqual({ "message": "Invalid request body" });
-// 	});
-// });
+	it("E2E Happy Path Journey - Incorrect Session Id", async () => {
+		const response = await postDocumentSelection(dataPassport, "sessionId");
+		expect(response.status).toBe(400);
+		expect(response.data).toEqual("Session id must be a valid uuid");
+	});
+});
 
 describe("Negative Path /userInfo Endpoint", () => {
 	let sessionId: any;
