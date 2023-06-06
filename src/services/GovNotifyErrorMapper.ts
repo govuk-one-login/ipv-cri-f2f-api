ace5c99632260f297a2eddb80c4dbb36e7753d46
Bhavana <bhavana.hemanth@digital.cabinet-office.gov.uk>
2023-06-06 15:43:57 +0100

    Removing commented lines and unwanted file.

--- src/services/GovNotifyErrorMapper.ts
+++ src/services/GovNotifyErrorMapper.ts
@@ -1,5 +1,3 @@
-// @ts-ignore
-import { find } from "lodash";
 import { AppError } from "../utils/AppError";
 import { HttpCodesEnum } from "../models/enums/HttpCodesEnum";
 
@@ -7,13 +5,6 @@ import { HttpCodesEnum } from "../models/enums/HttpCodesEnum";
  * Class to map error received from Gov Notify service to App Error object
  */
 export class GovNotifyErrorMapper {
-	//
-	// private static readonly errorCodeMap = new Map<number, AppCodes[]>([
-	// 	[ 400, [ AppCodes.E7408, AppCodes.E7409, AppCodes.E9511 ] ],
-	// 	[ 403, [ AppCodes.E8306, AppCodes.E5210, AppCodes.E9511 ] ],
-	// 	[ 429, [ AppCodes.W4101, AppCodes.E6101, AppCodes.E9511 ] ],
-	// 	[ 500, [ AppCodes.E5202] ],
-	// ]);
 
 	/**
 	 * Method that takes an error raised by SendEmailService and translates the error into an AppError with lambda specific AppCode
