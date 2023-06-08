3c7ebdd41374e9614fce55d0101bd380da9e6779
zmohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-06-08 18:08:54 +0100

    revert formatting change

--- src/services/YotiService.ts
+++ src/services/YotiService.ts
@@ -95,9 +95,19 @@ export class YotiService {
 			)}`;
 		}
 
-		const messageSignature = this.getRSASignatureForMessage(
-			`${method}&${endpointPath}${base64String}`,
-		);
+		let messageSignature;
+		try {
+			this.logger.info("Creating Yoti Message Signature")
+			messageSignature = this.getRSASignatureForMessage(
+				`${method}&${endpointPath}${base64String}`,
+			);
+			this.logger.info("Yoti Message Signature Created")
+		} catch (err) {
+			this.logger.error({ message: "An error occurred when creating Yoti message signature ", err });
+			throw new AppError(HttpCodesEnum.SERVER_ERROR, "Error create Yoti signature");
+		}
+		
+	
 
 		const config: AxiosRequestConfig<any> = {
 			headers: {
@@ -178,14 +188,14 @@ export class YotiService {
 		});
 
 		try {
-			this.logger.info("Yoti request url and config",{"yotiUrl":yotiRequest.url,"yotiConfig":yotiRequest.config});
+			this.logger.info("Yoti request url and config", { "yotiUrl":yotiRequest.url, "yotiConfig":yotiRequest.config });
 			const { data } = await axios.post(
 				yotiRequest.url,
 				payloadJSON,
 				yotiRequest.config,
 			);
 
-			this.logger.info("Received response for create /sessions",{"data":data})
+			this.logger.info("Received response for create /sessions", { data });
 			return data.session_id;
 		} catch (err) {
 			this.logger.error({ message: "An error occurred when creating Yoti session ", err });
