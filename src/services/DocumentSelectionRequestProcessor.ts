9359c10153013306c1a92441ba2dd512b1753cbd
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-06-16 15:24:37 +0100

    Adds extra data to F2F_YOTI_START TXMA message

--- src/services/DocumentSelectionRequestProcessor.ts
+++ src/services/DocumentSelectionRequestProcessor.ts
@@ -101,6 +101,10 @@ export class DocumentSelectionRequestProcessor {
 				await this.f2fService.sendToTXMA({
 					event_name: "F2F_YOTI_START",
 					...buildCoreEventFields(f2fSessionInfo, this.environmentVariables.issuer(), f2fSessionInfo.clientIpAddress, absoluteTimeNow),
+					document_type: selectedDocument,
+					issuing_country: countryCode,
+					post_office_details: postOfficeSelection,
+					yoti_session_id: yotiSessionId,
 				});
 			} catch (error) {
 				this.logger.error("Failed to write TXMA event F2F_YOTI_START to SQS queue.");
