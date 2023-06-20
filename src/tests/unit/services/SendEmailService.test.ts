ac65745d34ae0f7bf75c1baaa5aa610dc6a36521
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-06-20 10:55:48 +0100

    Adds test for F2F_YOTI_PDF_EMAILED, but timestamp fails

--- src/tests/unit/services/SendEmailService.test.ts
+++ src/tests/unit/services/SendEmailService.test.ts
@@ -81,7 +81,7 @@ describe("SendEmailProcessor", () => {
 
 		expect(mockGovNotify.sendEmail).toHaveBeenCalledTimes(1)
 		// eslint-disable-next-line @typescript-eslint/unbound-method
-		expect(mockF2fService.sendToTXMA).toHaveBeenCalledTimes(1);
+		expect(mockF2fService.sendToTXMA).toHaveBeenCalledWith(1, {"client_id": "ipv-core-stub", "component_id": "https://XXX-c.env.account.gov.uk", "event_name": "F2F_YOTI_PDF_EMAILED", "timestamp": "", "user": {"govuk_signin_journey_id": "sdfssg", "ip_address": "127.0.0.1", "persistent_session_id": "sdgsdg", "session_id": "sdfsdg", "transaction_id": undefined, "user_id": "sub"}, email: email.emailAddress});
 		expect(emailResponse.emailFailureMessage).toBe("");
 	});
 
