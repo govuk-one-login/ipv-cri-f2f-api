d473ee09ee8da645ece49fa5949073923ca56a55
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2023-06-28 10:57:55 +0100

    added messageCodes

--- src/YotiCallbackHandler.ts
+++ src/YotiCallbackHandler.ts
@@ -9,6 +9,9 @@ import { EnvironmentVariables } from "./services/EnvironmentVariables";
 import { YotiCallbackProcessor } from "./services/YotiCallbackProcessor";
 import { ServicesEnum } from "./models/enums/ServicesEnum";
 import { failEntireBatch, passEntireBatch } from "./utils/SqsBatchResponseHelper";
+import {MessageCodes} from "./models/enums/MessageCodes";
+import {Response} from "./utils/Response";
+import {HttpCodesEnum} from "./utils/HttpCodesEnum";
 
 const POWERTOOLS_METRICS_NAMESPACE = process.env.POWERTOOLS_METRICS_NAMESPACE ? process.env.POWERTOOLS_METRICS_NAMESPACE : Constants.F2F_METRICS_NAMESPACE;
 const POWERTOOLS_LOG_LEVEL = process.env.POWERTOOLS_LOG_LEVEL ? process.env.POWERTOOLS_LOG_LEVEL : Constants.DEBUG;
@@ -50,15 +53,21 @@ class YotiCallbackHandler implements LambdaInterface {
 					try {
 						YOTI_PRIVATE_KEY = await getParameter(this.environmentVariables.yotiKeySsmPath());
 					} catch (error) {
-						logger.error(`failed to get param from ssm at ${this.environmentVariables.yotiKeySsmPath()}`, { error });
-						throw error;
+						logger.error(`failed to get param from ssm at ${this.environmentVariables.yotiKeySsmPath()}`, {
+							messageCode: MessageCodes.MISSING_CONFIGURATION,
+							error,
+						});
+						return new Response(HttpCodesEnum.SERVER_ERROR, "An error has occurred");
 					}
 				}
 				
 				if (body.topic === "session_completion") {
 					await YotiCallbackProcessor.getInstance(logger, metrics, YOTI_PRIVATE_KEY).processRequest(body);
 				} else {
-					logger.warn("Unexpected topic received in request", { topic: body.topic });
+					logger.warn("Unexpected topic received in request", {
+						topic: body.topic,
+						messageCode: MessageCodes.UNEXPECTED_VENDOR_MESSAGE,
+					});
 					return failEntireBatch;
 				}
 				
@@ -72,16 +81,24 @@ class YotiCallbackHandler implements LambdaInterface {
 					error.obj = undefined;
 					batchFailures.push(new BatchItemFailure(record.messageId));
 					const sqsBatchResponse = { batchItemFailures: batchFailures };
-					logger.error("Returning batch item failure so it can be retried", { sqsBatchResponse });
+					logger.error("Returning batch item failure so it can be retried", {
+						sqsBatchResponse,
+						messageCode: MessageCodes.BATCH_PROCESSING_FAILURE,
+					});
 					return sqsBatchResponse;
 				} else {
-					logger.error({ message: "VC could not be sent. Returning failed message ", error });
+					logger.error({ message: "VC could not be sent. Returning failed message ",
+						error,
+						messageCode: MessageCodes.BATCH_PROCESSING_FAILURE,
+					});
 					return failEntireBatch;
 				}
 			}
 
 		} else {
-			logger.warn("Unexpected no of records received");
+			logger.warn("Unexpected no of records received",{
+				messageCode: MessageCodes.INCORRECT_BATCH_SIZE,
+			});
 			return failEntireBatch;
 		}
 	}
