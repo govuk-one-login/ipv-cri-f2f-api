0b694d2e1fe60a573c248542b3312da4cf30bd20
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-07-05 14:25:30 +0100

    Minor refactoring of unit tests

---
+++ src/tests/unit/data/txmaEvent.ts
@@ -0,0 +1,58 @@
+const TXMA_EXTENSION = {
+  extensions: {
+    evidence: [
+      {
+        "type": "IdentityCheck",
+        "strengthScore": 3,
+        "validityScore": 2,
+        "verificationScore": 3,
+        "checkDetails": [
+          {
+            "checkMethod": "vri",
+            "identityCheckPolicy": "published",
+            "txn": "b988e9c8-47c6-430c-9ca3-8cdacd85ee91",
+          },
+          {
+            "checkMethod": "pvr",
+            "photoVerificationProcessLevel": 3,
+            "txn": "b988e9c8-47c6-430c-9ca3-8cdacd85ee91",
+          },
+      ],
+      "ci": undefined,
+      }
+    ]
+  }
+}
+
+export const TXMA_CORE_FIELDS = {
+  "client_id": "ipv-core-stub", 
+  "component_id": "https://XXX-c.env.account.gov.uk", 
+  "event_name": "F2F_YOTI_RESPONSE_RECEIVED", 
+  "timestamp": 1, 
+  "user": {
+    "govuk_signin_journey_id": "sdfssg", 
+    "ip_address": "127.0.0.1", 
+    "persistent_session_id": "sdgsdg", 
+    "session_id": "RandomF2FSessionID", 
+    "transaction_id": undefined, 
+    "user_id": "testsub", 
+    "email": undefined
+  }
+}
+
+export const TXMA_VC_ISSUED = {
+  ...TXMA_CORE_FIELDS,
+  ...TXMA_EXTENSION, 
+  restricted: {
+    user: {
+      "name": "ANGELA ZOE UK SPECIMEN",
+      "birthDate": "1988-12-04"
+    },
+    "passport": [{
+      "documentType": "PASSPORT",
+      "documentNumber": "533401372",
+      "expiryDate": "2025-09-28",
+      "icaoIssuerCode": "GBR"
+    }]
+  }
+}
