6d4e3004ab377b19cf0382e1f0c2e99ad42dcb49
Bhavana <bhavana.hemanth@digital.cabinet-office.gov.uk>
2023-07-13 17:13:01 +0100

    Yoti stub to throw 400 response on create session when the address payload validation fails.

---
+++ yoti-stub/src/data/postSessions/postSessionsInvalidAddress400.ts
@@ -0,0 +1,9 @@
+export const POST_SESSIONS_INVALID_ADDRESS_400 = {
+    "code": "PAYLOAD_VALIDATION",
+    "errors": [
+        {
+            "property": "resources.applicant_profile.structured_postal_address",
+            "message": "INVALID_ADDRESS"
+        }
+    ]
+}
