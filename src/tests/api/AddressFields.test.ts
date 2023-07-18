32e7dcfb178ef459886c6ac887832cf886795c52
JasmineK19 <jasmine.kaur@digital.cabinet-office.gov.uk>
2023-07-18 14:54:28 +0100

    Fixed spacing

--- src/tests/api/AddressFields.test.ts
+++ src/tests/api/AddressFields.test.ts
@@ -7,15 +7,15 @@ describe("E2E Happy Path /documentSelection Endpoint", () => {
 	beforeEach(async () => {
         f2fStubPayload.yotiMockID = "0000";
         f2fStubPayload.target = `${constants.DEV_CRI_F2F_API_URL}`;
-        f2fStubPayload.shared_claims.address[0].uprn="123456789";
-        f2fStubPayload.shared_claims.address[0].addressLocality="Sidney";
-        f2fStubPayload.shared_claims.address[0].postalCode="F1 1SH";
-        f2fStubPayload.shared_claims.address[0].streetName="Wallaby Way";
+        f2fStubPayload.shared_claims.address[0].uprn = "123456789";
+        f2fStubPayload.shared_claims.address[0].addressLocality = "Sidney";
+        f2fStubPayload.shared_claims.address[0].postalCode = "F1 1SH";
+        f2fStubPayload.shared_claims.address[0].streetName = "Wallaby Way";
 	});
 	it("buildingName and subBuildingName missing", async () => {
-        f2fStubPayload.shared_claims.address[0].buildingNumber="32";
-        f2fStubPayload.shared_claims.address[0].buildingName="";
-        f2fStubPayload.shared_claims.address[0].subBuildingName="";
+        f2fStubPayload.shared_claims.address[0].buildingNumber = "32";
+        f2fStubPayload.shared_claims.address[0].buildingName = "";
+        f2fStubPayload.shared_claims.address[0].subBuildingName = "";
         console.log(JSON.stringify(f2fStubPayload));
         const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
 		sessionId = sessionResponse.data.session_id;
@@ -26,9 +26,9 @@ describe("E2E Happy Path /documentSelection Endpoint", () => {
 		expect(response.data).toBe("Instructions PDF Generated");
 	});
     it("buildingNumber missing", async () => {
-        f2fStubPayload.shared_claims.address[0].buildingNumber="";
-        f2fStubPayload.shared_claims.address[0].buildingName="19 A";
-        f2fStubPayload.shared_claims.address[0].subBuildingName="";
+        f2fStubPayload.shared_claims.address[0].buildingNumber = "";
+        f2fStubPayload.shared_claims.address[0].buildingName = "19 A";
+        f2fStubPayload.shared_claims.address[0].subBuildingName = "";
         console.log(JSON.stringify(f2fStubPayload));
         const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
 		sessionId = sessionResponse.data.session_id;
@@ -39,9 +39,9 @@ describe("E2E Happy Path /documentSelection Endpoint", () => {
 		expect(response.data).toBe("Instructions PDF Generated");
 	});
     it("buildingNumber and buildingName is missing", async () => {
-        f2fStubPayload.shared_claims.address[0].buildingNumber="";
-        f2fStubPayload.shared_claims.address[0].buildingName="";
-        f2fStubPayload.shared_claims.address[0].subBuildingName="Flat 5";
+        f2fStubPayload.shared_claims.address[0].buildingNumber = "";
+        f2fStubPayload.shared_claims.address[0].buildingName = "";
+        f2fStubPayload.shared_claims.address[0].subBuildingName = "Flat 5";
         console.log(JSON.stringify(f2fStubPayload));
         const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
 		sessionId = sessionResponse.data.session_id;
@@ -52,9 +52,9 @@ describe("E2E Happy Path /documentSelection Endpoint", () => {
 		expect(response.data).toBe("Instructions PDF Generated");
 	});
     it("buildingNumber is missing", async () => {
-        f2fStubPayload.shared_claims.address[0].buildingNumber="";
-        f2fStubPayload.shared_claims.address[0].buildingName="19 A";
-        f2fStubPayload.shared_claims.address[0].subBuildingName="Flat 5";
+        f2fStubPayload.shared_claims.address[0].buildingNumber = "";
+        f2fStubPayload.shared_claims.address[0].buildingName = "19 A";
+        f2fStubPayload.shared_claims.address[0].subBuildingName = "Flat 5";
         console.log(JSON.stringify(f2fStubPayload));
         const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
 		sessionId = sessionResponse.data.session_id;
@@ -65,10 +65,10 @@ describe("E2E Happy Path /documentSelection Endpoint", () => {
 		expect(response.data).toBe("Instructions PDF Generated");
 	});
 	it("incorrect country code", async () => {
-		f2fStubPayload.shared_claims.address[0].buildingNumber="";
-		f2fStubPayload.shared_claims.address[0].buildingName="19 A";
-		f2fStubPayload.shared_claims.address[0].subBuildingName="";
-		f2fStubPayload.shared_claims.address[0].addressCountry="XYZ"
+		f2fStubPayload.shared_claims.address[0].buildingNumber = "";
+		f2fStubPayload.shared_claims.address[0].buildingName = "19 A";
+		f2fStubPayload.shared_claims.address[0].subBuildingName = "";
+		f2fStubPayload.shared_claims.address[0].addressCountry = "XYZ"
 		console.log(JSON.stringify(f2fStubPayload));
 		const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
 		sessionId = sessionResponse.data.session_id;
@@ -80,9 +80,9 @@ describe("E2E Happy Path /documentSelection Endpoint", () => {
 	});
 
 	it("buildingNumber, buildingName and subBuildingName- all missing", async () => {
-		f2fStubPayload.shared_claims.address[0].buildingNumber="";
-		f2fStubPayload.shared_claims.address[0].buildingName="";
-		f2fStubPayload.shared_claims.address[0].subBuildingName="";
+		f2fStubPayload.shared_claims.address[0].buildingNumber = "";
+		f2fStubPayload.shared_claims.address[0].buildingName = "";
+		f2fStubPayload.shared_claims.address[0].subBuildingName = "";
 		console.log(JSON.stringify(f2fStubPayload));
 		const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
 		sessionId = sessionResponse.data.session_id;
