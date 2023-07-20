2a8c3ccd107d206d4207605ffec68ce61a608729
mudassar jafer <mjafer@deloitte.co.uk>
2023-07-20 12:28:33 +0100

    Test container shell script update

---
+++ run-tests.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -eu
+
+remove_quotes () {
+  echo "$1" | tr -d '"'
+}
+
+declare error_code
+
+#The CFN variables seem to include quotes when used in tests these must be removed before assigning them.
+#CFN_F2FBackendURL_NoQuotes=$(remove_quotes "$CFN_F2FBackendURL")
+#export DEV_CRI_F2F_API_URL=$(echo ${CFN_F2FBackendURL_NoQuotes%/})
+# shellcheck disable=SC2154
+export DEV_CRI_F2F_API_URL=$(remove_quotes "$CFN_F2FBackendURL")
+# shellcheck disable=SC2154
+export DEV_IPV_F2F_STUB_URL=$(remove_quotes "$CFN_F2FIPVStubExecuteURL")
+export DEV_F2F_EXPIRED_ACCESS_TOKEN=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImY0NWNkMzM4LTRmY2MtNDM2OS05NDBlLTg2ZjJiMDk1OTFjNyJ9.eyJzdWIiOiI2Y2QwYTk2My00Y2VhLTQ2NDUtOWE3NC05Y2QwODRiNzMxYmYiLCJhdWQiOiJodHRwczovL3Jldmlldy1vLmRldi5hY2NvdW50Lmdvdi51ayIsImlzcyI6Imh0dHBzOi8vcmV2aWV3LW8uZGV2LmFjY291bnQuZ292LnVrIiwiZXhwIjoxNjg0NDEzMDkyfQ.ZKrmFiBu5Ui0t4E0h17H8QPELE1V3FjeO7EQ-SF2-bYnmLfNTjCLvqjMrIgm5_fPDRCFU1N327VVMwHjQWzojw
+export DEV_F2F_MISSING_SUB_ACCESS_TOKEN=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImY0NWNkMzM4LTRmY2MtNDM2OS05NDBlLTg2ZjJiMDk1OTFjNyJ9.eyJhdWQiOiJodHRwczovL3Jldmlldy1vLmRldi5hY2NvdW50Lmdvdi51ayIsImlzcyI6Imh0dHBzOi8vcmV2aWV3LW8uZGV2LmFjY291bnQuZ292LnVrIiwiZXhwIjoxNjg0NDQ2NjQwfQ.qPRn-DWRUv-prYyZvWfV9wWkoCOQYw9iwZgpvDmX9Vic8McJp2rxO2rKdpAOtewgcUsV3enHz1-ewZ-kaSxidw
+# shellcheck disable=SC2154
+export DEV_F2F_YOTI_STUB_URL=$(remove_quotes "$CFN_F2FYotiStubURL")
+
+echo "F2F backend URL $DEV_CRI_F2F_API_URL"
+echo $DEV_IPV_F2F_STUB_URL
+echo $DEV_F2F_EXPIRED_ACCESS_TOKEN
+echo $DEV_F2F_MISSING_SUB_ACCESS_TOKEN
+echo $DEV_F2F_YOTI_STUB_URL
+
+cd src; npm run test:api
+error_code=$?
+
+cp -rf results $TEST_REPORT_ABSOLUTE_DIR
+
+exit $error_code
