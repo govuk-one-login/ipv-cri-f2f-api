503dccc7af23fd1a996b7f988652d50a323cda85
Daniel Page <danielpage@deloitte.co.uk>
2023-07-24 16:36:05 +0100

    abort test payload updated

--- src/tests/api/AbortApi.test.ts
+++ src/tests/api/AbortApi.test.ts
@@ -15,7 +15,7 @@ describe("E2E Happy Path /abort enpoint", () => {
 	it("E2E Happy Path Journey - Abort Session", async () => {
 		expect(sessionId).toBeTruthy();
 		const response = await postAbortSession(abortPayload, sessionId);
-		expect(response.request.path).toContain("/redirect?error=access_denied&state=F2F_CRI_SESSION_ABORTED");
+		expect(response.request.path).toContain("/redirect?error=access_denied&state=");
 	});
 
 	it("E2E Happy Path Journey - Abort Previously Aborted Session", async () => {
