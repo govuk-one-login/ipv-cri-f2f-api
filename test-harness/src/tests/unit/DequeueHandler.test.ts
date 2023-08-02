0378de272184274c12e3069d4b61a5100923fa83
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2023-08-02 17:32:07 +0100

    [F2F-1006] Tidy up object names

--- test-harness/src/tests/unit/DequeueHandler.test.ts
+++ test-harness/src/tests/unit/DequeueHandler.test.ts
@@ -19,9 +19,6 @@ jest.mock("@aws-lambda-powertools/logger", () => ({
 	})),
 }));
 
-process.env.BUCKET_FOLDER_PREFIX = "ipv-core/"
-process.env.EVENT_TEST_BUCKET_NAME = "test-bucket";
-
 describe("DequeueHandler", () => {
   const body1 = JSON.stringify({
     event_name: "TEST_EVENT",
@@ -38,13 +35,50 @@ describe("DequeueHandler", () => {
     ],
   };
 
-  it("Returns no batchItemFailures if all events were successfully sent to S3", async () => {
+  beforeEach(() => {
+    process.env.BUCKET_FOLDER_PREFIX = "ipv-core/";
+    process.env.EVENT_TEST_BUCKET_NAME = "test-bucket";
+    process.env.PROPERTY_NAME = "sub";
+  })
+
+  it("Returns no batchItemFailures if all events were successfully sent to S3 where property name is sub", async () => {
     jest.spyOn(s3Client, 'send').mockReturnValueOnce();
 
     const result = await lambdaHandler(event);
     expect(logger.info).toHaveBeenCalledWith("Starting to process records");
-    expect(logger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_SUB_1-1577836800-11111 to bucket test-bucket`);
-    expect(logger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_SUB_2-1577836800-22222 to bucket test-bucket`);
+    expect(logger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_SUB_1-2020-01-01T00:00:00.000Z-11111 to bucket test-bucket`);
+    expect(logger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_SUB_2-2020-01-01T00:00:00.000Z-22222 to bucket test-bucket`);
+    expect(logger.info).toHaveBeenCalledWith("Finished processing records");
+    expect(result).toEqual({ batchItemFailures: [] });
+  });
+
+  it.only("Returns no batchItemFailures if all events were successfully sent to S3 where property name is user.session_id", async () => {
+    process.env.PROPERTY_NAME = "user.session_id";
+    const body1 = JSON.stringify({
+      event_name: "F2F_YOTI_START",
+      user: {
+        session_id: "test_user_id_1"
+      }
+    });
+    const body2 = JSON.stringify({
+      event_name: "F2F_YOTI_START",
+      user: {
+        session_id: "test_user_id_2"
+      }
+    });
+    const txmaEvent = {
+      Records: [
+        { messageId: '11111', body: body1 },
+        { messageId: '22222', body: body2 },
+      ],
+    };
+    
+    jest.spyOn(s3Client, 'send').mockReturnValueOnce();
+
+    const result = await lambdaHandler(txmaEvent);
+    expect(logger.info).toHaveBeenCalledWith("Starting to process records");
+    expect(logger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_user_id_1-2020-01-01T00:00:00.000Z-11111 to bucket test-bucket`);
+    expect(logger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_user_id_2-2020-01-01T00:00:00.000Z-22222 to bucket test-bucket`);
     expect(logger.info).toHaveBeenCalledWith("Finished processing records");
     expect(result).toEqual({ batchItemFailures: [] });
   });
