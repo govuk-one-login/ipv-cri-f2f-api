54d96577068bdbcbb9b5788a34e5d4e1007155cf
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-08-07 12:49:17 +0100

    wip

--- test-harness/src/DequeueHandler.ts
+++ test-harness/src/DequeueHandler.ts
@@ -5,47 +5,58 @@ import { NodeHttpHandler } from "@aws-sdk/node-http-handler";
 import { Constants } from "./utils/Constants";
 import { BatchItemFailure } from "./utils/BatchItemFailure";
 
-const {
-  POWERTOOLS_LOG_LEVEL = Constants.DEBUG,
-  POWERTOOLS_SERVICE_NAME = Constants.DEQUEUE_LOGGER_SVC_NAME,
-  REGION,
-  EVENT_TEST_BUCKET_NAME,
-  PROPERTY_NAME = "sub",
-  BUCKET_FOLDER_PREFIX,
-} = process.env;
+const POWERTOOLS_LOG_LEVEL = process.env.POWERTOOLS_LOG_LEVEL ? process.env.POWERTOOLS_LOG_LEVEL : Constants.DEBUG;
+const POWERTOOLS_SERVICE_NAME = process.env.POWERTOOLS_SERVICE_NAME ? process.env.POWERTOOLS_SERVICE_NAME : Constants.DEQUEUE_LOGGER_SVC_NAME;
 
-const logger = new Logger({ logLevel: POWERTOOLS_LOG_LEVEL, serviceName: POWERTOOLS_SERVICE_NAME });
+export const logger = new Logger({
+	logLevel: POWERTOOLS_LOG_LEVEL,
+	serviceName: POWERTOOLS_SERVICE_NAME,
+});
 
-const s3Client = new S3Client({
-  region: REGION,
-  maxAttempts: 2,
-  requestHandler: new NodeHttpHandler({ connectionTimeout: 29000, socketTimeout: 29000 }),
+export const s3Client = new S3Client({
+	region: process.env.REGION,
+	maxAttempts: 2,
+	requestHandler: new NodeHttpHandler({
+		connectionTimeout: 29000,
+		socketTimeout: 29000,
+	}),
 });
 
 class DequeueHandler implements LambdaInterface {
-  async handler(event: any): Promise<any> {
-    logger.info("Starting to process records");
-    const batchFailures: BatchItemFailure[] = [];
+	async handler(event: any): Promise<any> {
+		logger.info("Starting to process records");
+		const batchFailures: BatchItemFailure[] = [];
 
-    for await (const { body, messageId } of event.Records) {
-      const parsedBody = JSON.parse(body);
-      const propertyValue = PROPERTY_NAME.split('.').reduce((acc, key) => acc[key], parsedBody);
-      const timestamp = new Date().toISOString();
-      const key = `${BUCKET_FOLDER_PREFIX}${propertyValue}-${timestamp}-${messageId}`;
-      const uploadParams = { Bucket: EVENT_TEST_BUCKET_NAME, Key: key, Body: JSON.stringify(parsedBody), ContentType: "application/json" };
+		for await (const record of event.Records) {
+			const body = JSON.parse(record.body);
+			const bucket = process.env.EVENT_TEST_BUCKET_NAME;
+			const propertyName = process.env.PROPERTY_NAME || "sub";
+			// Accounts for values that are nested (eg user.session_id)
+			const propertyValue = propertyName.split('.').reduce((acc, key) => acc[key], body)
+			const folder = process.env.BUCKET_FOLDER_PREFIX;
+			const timestamp = new Date().toISOString();
+			const key = `${folder}${propertyValue}-${timestamp}-${record.messageId}`;
 
-      try {
-        logger.info(`Uploading object with key ${key} to bucket ${EVENT_TEST_BUCKET_NAME}`);
-        await s3Client.send(new PutObjectCommand(uploadParams));
-      } catch (error) {
-        batchFailures.push(new BatchItemFailure(messageId));
-        logger.error({ message: "Error writing keys to S3 bucket", error });
-      }
-    }
+			const uploadParams = {
+				Bucket: bucket,
+				Key: key,
+				Body: JSON.stringify(body),
+				ContentType: "application/json",
+			};
+			
+			try {
+				logger.info(`Uploading object with key ${key} to bucket ${bucket}`);
+				await s3Client.send(new PutObjectCommand(uploadParams));
+			} catch (error) {
+				batchFailures.push(new BatchItemFailure(record.messageId));
+				logger.error({ message: "Error writing keys to S3 bucket", error });
+			}
+		};
 
-    logger.info("Finished processing records");
-    return { batchItemFailures: batchFailures };
+		logger.info("Finished processing records")
+		return { batchItemFailures: batchFailures };
   }
 }
 
-export const lambdaHandler = new DequeueHandler().handler.bind(DequeueHandler);
+const handlerClass = new DequeueHandler();
+export const lambdaHandler = handlerClass.handler.bind(handlerClass);
--- test-harness/src/tests/unit/DequeueHandler.test.ts
+++ test-harness/src/tests/unit/DequeueHandler.test.ts
@@ -1,8 +1,5 @@
-import { mock } from "jest-mock-extended";
-import { lambdaHandler } from "../../DequeueHandler";
+import { lambdaHandler, logger, s3Client } from "../../DequeueHandler";
 import { BatchItemFailure } from "../../utils/BatchItemFailure";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { Logger } from "@aws-lambda-powertools/logger";
 
 jest
   .useFakeTimers()
@@ -10,79 +7,89 @@ jest
 
 jest.mock("@aws-sdk/client-s3", () => ({
 	S3Client: jest.fn().mockImplementation(() => ({
-		send: jest.fn()
+		send: jest.fn(),
 	})),
-	PutObjectCommand: PutObjectCommand
+	PutObjectCommand: jest.fn().mockImplementation(() => ({})),
 }));
 
 jest.mock("@aws-lambda-powertools/logger", () => ({
 	Logger: jest.fn().mockImplementation(() => ({
 		info: jest.fn(),
-		error: jest.fn()
+		error: jest.fn(),
 	})),
 }));
 
-const mockS3Client = new S3Client({});
-const mockLogger = new Logger({});
-
 describe("DequeueHandler", () => {
-	// beforeEach(() => {
-	// 	jest
-	// 	.useFakeTimers()
-	// 	.setSystemTime(new Date('2020-01-01'));
-  // });
-
-	// afterEach(() => {
-  //   jest.useRealTimers();
-  //   jest.resetAllMocks();
-  // });
-
-  const body1 = JSON.stringify({ event_name: "TEST_EVENT", sub: "test_SUB_1" });
-  const body2 = JSON.stringify({ event_name: "TEST_EVENT", sub: "test_SUB_2" });
-  const event = { Records: [{ messageId: '11111', body: body1 }, { messageId: '22222', body: body2 }] };
+  const body1 = JSON.stringify({
+    event_name: "TEST_EVENT",
+    sub: "test_SUB_1",
+  });
+  const body2 = JSON.stringify({
+    event_name: "TEST_EVENT",
+    sub: "test_SUB_2",
+  });
+  const event = {
+    Records: [
+      { messageId: '11111', body: body1 },
+      { messageId: '22222', body: body2 },
+    ],
+  };
 
   beforeEach(() => {
     process.env.BUCKET_FOLDER_PREFIX = "ipv-core/";
     process.env.EVENT_TEST_BUCKET_NAME = "test-bucket";
     process.env.PROPERTY_NAME = "sub";
-    mockS3Client.send.mockClear();
-    mockLogger.info.mockClear();
-    mockLogger.error.mockClear();
   })
 
   it("Returns no batchItemFailures if all events were successfully sent to S3 where property name is sub", async () => {
-    await expect(lambdaHandler(event)).resolves.toEqual({ batchItemFailures: [] });
+    jest.spyOn(s3Client, 'send').mockReturnValueOnce();
 
-    expect(mockLogger.info).toHaveBeenCalledWith("Starting to process records");
-    expect(mockLogger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_SUB_1-2020-01-01T00:00:00.000Z-11111 to bucket test-bucket`);
-    expect(mockLogger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_SUB_2-2020-01-01T00:00:00.000Z-22222 to bucket test-bucket`);
-    expect(mockLogger.info).toHaveBeenCalledWith("Finished processing records");
-    expect(mockS3Client.send).toHaveBeenCalledTimes(2);
+    const result = await lambdaHandler(event);
+    expect(logger.info).toHaveBeenCalledWith("Starting to process records");
+    expect(logger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_SUB_1-2020-01-01T00:00:00.000Z-11111 to bucket test-bucket`);
+    expect(logger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_SUB_2-2020-01-01T00:00:00.000Z-22222 to bucket test-bucket`);
+    expect(logger.info).toHaveBeenCalledWith("Finished processing records");
+    expect(result).toEqual({ batchItemFailures: [] });
   });
 
   it("Returns no batchItemFailures if all events were successfully sent to S3 where property name is user.session_id", async () => {
     process.env.PROPERTY_NAME = "user.session_id";
-    const body1 = JSON.stringify({ event_name: "F2F_YOTI_START", user: { session_id: "test_user_id_1" } });
-    const body2 = JSON.stringify({ event_name: "F2F_YOTI_START", user: { session_id: "test_user_id_2" } });
-    const txmaEvent = { Records: [{ messageId: '11111', body: body1 }, { messageId: '22222', body: body2 }] };
+    const body1 = JSON.stringify({
+      event_name: "F2F_YOTI_START",
+      user: {
+        session_id: "test_user_id_1"
+      }
+    });
+    const body2 = JSON.stringify({
+      event_name: "F2F_YOTI_START",
+      user: {
+        session_id: "test_user_id_2"
+      }
+    });
+    const txmaEvent = {
+      Records: [
+        { messageId: '11111', body: body1 },
+        { messageId: '22222', body: body2 },
+      ],
+    };
+    
+    jest.spyOn(s3Client, 'send').mockReturnValueOnce();
 
-    await expect(lambdaHandler(txmaEvent)).resolves.toEqual({ batchItemFailures: [] });
-
-    expect(mockLogger.info).toHaveBeenCalledWith("Starting to process records");
-    expect(mockLogger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_user_id_1-2020-01-01T00:00:00.000Z-11111 to bucket test-bucket`);
-    expect(mockLogger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_user_id_2-2020-01-01T00:00:00.000Z-22222 to bucket test-bucket`);
-    expect(mockLogger.info).toHaveBeenCalledWith("Finished processing records");
-    expect(mockS3Client.send).toHaveBeenCalledTimes(2);
+    const result = await lambdaHandler(txmaEvent);
+    expect(logger.info).toHaveBeenCalledWith("Starting to process records");
+    expect(logger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_user_id_1-2020-01-01T00:00:00.000Z-11111 to bucket test-bucket`);
+    expect(logger.info).toHaveBeenCalledWith(`Uploading object with key ipv-core/test_user_id_2-2020-01-01T00:00:00.000Z-22222 to bucket test-bucket`);
+    expect(logger.info).toHaveBeenCalledWith("Finished processing records");
+    expect(result).toEqual({ batchItemFailures: [] });
   });
 
   it("Returns batchItemFailures if events failed to send to S3", async () => {
-    const error = new Error("Failed to send to S3");
-    mockS3Client.send.mockImplementationOnce(() => { throw error });
+    const error = new Error("Failed to send to S3")
+    jest.spyOn(s3Client, 'send').mockImplementationOnce(() => { throw error });
 
-    await expect(lambdaHandler(event)).resolves.toEqual({ batchItemFailures: [new BatchItemFailure("11111")] });
-
-    expect(mockLogger.error).toHaveBeenCalledWith({ message: "Error writing keys to S3 bucket", error });
-    expect(mockLogger.info).toHaveBeenCalledWith("Finished processing records");
-    expect(mockS3Client.send).toHaveBeenCalledTimes(1);
+    const result = await lambdaHandler(event);
+    expect(logger.error).toHaveBeenCalledWith({ message: "Error writing keys to S3 bucket", error })
+    expect(logger.info).toHaveBeenCalledWith("Finished processing records");
+    expect(result).toEqual({ batchItemFailures: [new BatchItemFailure("11111")] });
   });
 });
