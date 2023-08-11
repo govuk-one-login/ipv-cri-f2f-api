f99785dd2dbfc440c29efcecab4ee9d61c13cd3d
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-08-11 08:54:47 +0100

    F2F-1000 test harness updates

--- src/tests/api/CallbackApi.test.ts
+++ src/tests/api/CallbackApi.test.ts
@@ -17,8 +17,8 @@ import {
 	userInfoPost,
 	validateJwtToken,
 	getDequeuedSqsMessage,
-	getSqsEventList,
 	validateTxMAEventData,
+	validateJwtTokenNamePart,
 } from "../utils/ApiTestSteps";
 import "dotenv/config";
 import { constants } from "../utils/ApiConstants";
@@ -69,15 +69,52 @@ describe("Callback API", () => {
 
 	}, 20000);
 
+	it("F2F CRI Callback Endpoint Integration HappyPath - yotiMockId: '%s'", async () => {
+		f2fStubPayload.yotiMockID = "0000";
+		const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
+		const sessionId = sessionResponse.data.session_id;
+		const sub = sessionResponse.data.sub;
+
+		// Document Selection
+		const response = await postDocumentSelection(dataPassport, sessionId);
+		expect(response.status).toBe(200);
+		// Authorization
+		const authResponse = await authorizationGet(sessionId);
+		expect(authResponse.status).toBe(200);
+		// // Post Token
+		const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
+		expect(tokenResponse.status).toBe(200);
+		// Post User Info
+		const userInfoResponse = await userInfoPost("Bearer " + tokenResponse.data.access_token);
+		expect(userInfoResponse.status).toBe(202);
+
+		// Get Yoti Session Id
+		const session = await getSessionById(sessionId, constants.DEV_F2F_SESSION_TABLE_NAME);
+		const yotiSessionId: any = session?.yotiSessionId;
+		console.log(yotiSessionId);
+
+		// Yoti Callback
+		const callbackResponse = await callbackPost(yotiSessionId);
+		expect(callbackResponse.status).toBe(202);
+
+		// Retrieve Verifiable Credential from dequeued SQS queue
+		let sqsMessage;
+		do {
+			sqsMessage = await getDequeuedSqsMessage(sub);
+		} while (!sqsMessage);
+		const jwtToken = sqsMessage["https://vocab.account.gov.uk/v1/credentialJWT"][0];
+
+		validateJwtToken(jwtToken, vcResponseData, "0000");
+	}, 20000);
+
 	it.each([
-		["0150", dataPassport, "FRANK", "JACOB", "JAMES", "SMITH0150"],
-		["0151", dataPassport, "FRANK", "JACOB", "JAMES", "SMITH0151"],
-		["0152", dataPassport, "FRANK", "JACOB", "JAMES", "SMITH0152"],
-	])("F2F CRI Callback Endpoint - yotiMockId: '%s'", async (yotiMockId: string, docSelectionData:any, givenName1:any, givenName2:any, givenName3:any, familyName:any ) => {
+		["0150", dataPassport, "FRANK", "JACOB", "JAMES", "SMITH"],
+		["0151", dataPassport,  "FRANK", "JACOB", "JAMES", "SMITH"],
+		["0152", dataPassport,  "FRANK", "JACOB", "JAMES", "SMITH"],
+	])("F2F CRI Callback Endpoint Given Names in Yoti Response- yotiMockId: '%s'", async (yotiMockId: string, docSelectionData:any, givenName1:any, givenName2:any, givenName3:any, familyName:any ) => {
 		f2fStubPayload.yotiMockID = yotiMockId;
 		f2fStubPayload.shared_claims.name[0].nameParts[0].value = givenName1;
-		f2fStubPayload.shared_claims.name[0].nameParts[1].value = givenName2;
-	 	f2fStubPayload.shared_claims.name[0].nameParts[2].value = givenName2;
+		f2fStubPayload.shared_claims.name[0].nameParts[1].value = givenName2 + " " + givenName3;
 		f2fStubPayload.shared_claims.name[0].nameParts[2].value = familyName;
 
 		const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
@@ -98,12 +135,13 @@ describe("Callback API", () => {
 		expect(userInfoResponse.status).toBe(202);
 
 		// Get Yoti Session Id
-		// const session = await getSessionById(sessionId, "session-f2f-cri-ddb");
-		// const yotiSessionId: any = session?.yotiSessionId;
-		// console.log(yotiSessionId);
+		const session = await getSessionById(sessionId, constants.DEV_F2F_SESSION_TABLE_NAME);
+		const yotiSessionId: any = session?.yotiSessionId;
+		console.log(yotiSessionId);
+
 		// Yoti Callback
 		const callbackResponse = await callbackPost(yotiSessionId);
-		expect(callbackResponse.status).toBe(202);
+		console.log(callbackResponse.data);
 
 		// Retrieve Verifiable Credential from dequeued SQS queue
 		let sqsMessage;
@@ -111,8 +149,7 @@ describe("Callback API", () => {
 			sqsMessage = await getDequeuedSqsMessage(sub);
 		} while (!sqsMessage);
 		const jwtToken = sqsMessage["https://vocab.account.gov.uk/v1/credentialJWT"][0];
-
-		validateJwtToken(jwtToken, vcResponseData, "0000");
+		validateJwtTokenNamePart(jwtToken, givenName1, givenName2, givenName3, familyName + yotiMockId);
 	}, 20000);
 
 	it("F2F CRI Callback Endpoint TxMA Validation", async () => {
