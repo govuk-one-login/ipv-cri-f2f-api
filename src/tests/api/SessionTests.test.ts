b2f9be34283c7efcec7c0c1ac09245afef0f4dfa
JasmineK19 <jasmine.kaur@digital.cabinet-office.gov.uk>
2023-08-14 11:50:55 +0100

    F2F-993 SessionAddressTests

---
+++ src/tests/api/SessionTests.test.ts
@@ -0,0 +1,38 @@
+import { sessionPost, startStubServiceAndReturnSessionId, stubStartPost } from "../utils/ApiTestSteps";
+import f2fStubPayload from "../data/exampleStubPayload.json";
+import addressSessionPayload from "../data/addressSessionPayload.json";
+import exampleStubPayload from "../data/exampleStubPayload.json";
+
+describe("Address fields tests", () => {
+    let sessionId: any;
+    beforeEach(async () => {
+        f2fStubPayload.yotiMockID = "0000";
+        f2fStubPayload.shared_claims.address[0].uprn = "123456789";
+        f2fStubPayload.shared_claims.address[0].addressLocality = "Sidney";
+        f2fStubPayload.shared_claims.address[0].postalCode = "F1 1SH";
+        f2fStubPayload.shared_claims.address[0].streetName = "Wallaby Way";
+    });
+
+    it("Correct address format", async () => {
+        console.log(JSON.stringify(exampleStubPayload));
+        const stubResponse = await stubStartPost(f2fStubPayload);
+        const postRequest = await sessionPost(stubResponse.data.clientId, stubResponse.data.request);
+        expect(postRequest.status).toBe(400);
+    });
+
+    it("country is not GB", async () => {
+        f2fStubPayload.shared_claims.address[0].addressCountry = "XY"
+        console.log(JSON.stringify(f2fStubPayload));
+        const stubResponse = await stubStartPost(f2fStubPayload);
+        const postRequest = await sessionPost(stubResponse.data.clientId, stubResponse.data.request);
+        expect(postRequest.status).toBe(400);
+    });
+
+    it("Incorrect address format", async () => {
+        console.log(JSON.stringify(addressSessionPayload));
+        const stubResponse = await stubStartPost(f2fStubPayload);
+        const postRequest = await sessionPost(stubResponse.data.clientId, stubResponse.data.request);
+        expect(postRequest.status).toBe(400);
+    });
+
+});
