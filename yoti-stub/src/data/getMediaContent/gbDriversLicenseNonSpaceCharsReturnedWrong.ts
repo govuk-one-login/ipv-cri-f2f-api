89f92e4bad21a3d8a32ce4d22f4df78b8c464938
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2023-08-15 12:46:11 +0100

    4 new cases added for name cases

---
+++ yoti-stub/src/data/getMediaContent/gbDriversLicenseNonSpaceCharsReturnedWrong.ts
@@ -0,0 +1,26 @@
+export const GBR_DRIVING_LICENCE_NON_SPACE_CHARS_RETURNED_WRONG = {
+	"full_name": "FREDERICK DOM FLINTSTONE",
+	"date_of_birth": "1976-03-11",
+	"given_names": "FREDERICK DOM",
+	"family_name": "FLINTSTONE",
+	"place_of_birth": "UNITED KINGDOM",
+	"gender": "FEMALE",
+	"structured_postal_address": {
+			"address_format": 1,
+			"building_number": "122",
+			"address_line1": "122 BURNS CRESCENT",
+			"address_line2": "EDINBURGH",
+			"address_line3": "EH1 9GP",
+			"town_city": "EDINBURGH",
+			"postal_code": "EH1 9GP",
+			"country_iso": "GBR",
+			"country": "United Kingdom",
+			"formatted_address": "122 BURNS CRESCENT\nEDINBURGH\nEH1 9GP"
+	},
+	"document_type": "DRIVING_LICENCE",
+	"issuing_country": "GBR",
+	"document_number": "MORGA753116SM9IJ35",
+	"expiration_date": "2023-01-18",
+	"date_of_issue": "2013-01-19",
+	"issuing_authority": "DVLA"
+}
