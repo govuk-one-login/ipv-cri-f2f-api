cd2ad78b1db93ad015fc382e6008d67f1bdaef1c
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-08-21 17:42:43 +0100

    uncomment tests

--- src/tests/api/CallbackApi.test.ts
+++ src/tests/api/CallbackApi.test.ts
@@ -107,126 +107,126 @@ describe("Callback API", () => {
 
 	}, 20000);
 
-	// it("F2F CRI Callback Endpoint Integration HappyPath - yotiMockId: '%s'", async () => {
-	// 	f2fStubPayload.yotiMockID = "0000";
-	// 	const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
-	// 	const sessionId = sessionResponse.data.session_id;
-	// 	const sub = sessionResponse.data.sub;
+	it("F2F CRI Callback Endpoint Integration HappyPath - yotiMockId: '%s'", async () => {
+		f2fStubPayload.yotiMockID = "0000";
+		const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
+		const sessionId = sessionResponse.data.session_id;
+		const sub = sessionResponse.data.sub;
 
-	// 	// Document Selection
-	// 	const response = await postDocumentSelection(dataPassport, sessionId);
-	// 	expect(response.status).toBe(200);
-	// 	// Authorization
-	// 	const authResponse = await authorizationGet(sessionId);
-	// 	expect(authResponse.status).toBe(200);
-	// 	// // Post Token
-	// 	const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
-	// 	expect(tokenResponse.status).toBe(200);
-	// 	// Post User Info
-	// 	const userInfoResponse = await userInfoPost("Bearer " + tokenResponse.data.access_token);
-	// 	expect(userInfoResponse.status).toBe(202);
+		// Document Selection
+		const response = await postDocumentSelection(dataPassport, sessionId);
+		expect(response.status).toBe(200);
+		// Authorization
+		const authResponse = await authorizationGet(sessionId);
+		expect(authResponse.status).toBe(200);
+		// // Post Token
+		const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
+		expect(tokenResponse.status).toBe(200);
+		// Post User Info
+		const userInfoResponse = await userInfoPost("Bearer " + tokenResponse.data.access_token);
+		expect(userInfoResponse.status).toBe(202);
 
-	// 	// Get Yoti Session Id
-	// 	const session = await getSessionById(sessionId, constants.DEV_F2F_SESSION_TABLE_NAME);
-	// 	const yotiSessionId: any = session?.yotiSessionId;
-	// 	console.log(yotiSessionId);
+		// Get Yoti Session Id
+		const session = await getSessionById(sessionId, constants.DEV_F2F_SESSION_TABLE_NAME);
+		const yotiSessionId: any = session?.yotiSessionId;
+		console.log(yotiSessionId);
 
-	// 	// Yoti Callback
-	// 	const callbackResponse = await callbackPost(yotiSessionId);
-	// 	expect(callbackResponse.status).toBe(202);
+		// Yoti Callback
+		const callbackResponse = await callbackPost(yotiSessionId);
+		expect(callbackResponse.status).toBe(202);
 
-	// 	// Retrieve Verifiable Credential from dequeued SQS queue
-	// 	let sqsMessage;
-	// 	do {
-	// 		sqsMessage = await getDequeuedSqsMessage(sub);
-	// 	} while (!sqsMessage);
-	// 	const jwtToken = sqsMessage["https://vocab.account.gov.uk/v1/credentialJWT"][0];
+		// Retrieve Verifiable Credential from dequeued SQS queue
+		let sqsMessage;
+		do {
+			sqsMessage = await getDequeuedSqsMessage(sub);
+		} while (!sqsMessage);
+		const jwtToken = sqsMessage["https://vocab.account.gov.uk/v1/credentialJWT"][0];
 
-	// 	validateJwtToken(jwtToken, vcResponseData, "0000");
-	// }, 20000);
+		validateJwtToken(jwtToken, vcResponseData, "0000");
+	}, 20000);
 
-	// it.each([
-	// 	["0150", dataPassport, "FRANK", "JACOB", "JAMES", "SMITH"],
-	// 	["0151", dataPassport, "FRANK", "JACOB", "JAMES", "SMITH"],
-	// 	["0152", dataPassport, "FRANK", "JACOB", "JAMES", "SMITH"],
-	// 	["0402", dataEuDrivingLicence, "FREDERICK", "SMITH", "JON", "FLINTSTONE"],
-	// 	["0206", dataNonUkPassport, "FREDERICK", "JON", "De", "FLINTSTONE"],
+	it.each([
+		["0150", dataPassport, "FRANK", "JACOB", "JAMES", "SMITH"],
+		["0151", dataPassport, "FRANK", "JACOB", "JAMES", "SMITH"],
+		["0152", dataPassport, "FRANK", "JACOB", "JAMES", "SMITH"],
+		["0402", dataEuDrivingLicence, "FREDERICK", "SMITH", "JON", "FLINTSTONE"],
+		["0206", dataNonUkPassport, "FREDERICK", "JON", "De", "FLINTSTONE"],
 
-	// ])("F2F CRI Callback Endpoint Given Names in Yoti Response- yotiMockId: '%s'", async (yotiMockId: string, docSelectionData:any, givenName1:any, givenName2:any, givenName3:any, familyName:any ) => {
-	// 	f2fStubPayload.yotiMockID = yotiMockId;
-	// 	f2fStubPayload.shared_claims.name[0].nameParts[0].value = givenName1;
-	// 	f2fStubPayload.shared_claims.name[0].nameParts[1].value = givenName2 + " " + givenName3;
-	// 	f2fStubPayload.shared_claims.name[0].nameParts[2].value = familyName;
+	])("F2F CRI Callback Endpoint Given Names in Yoti Response- yotiMockId: '%s'", async (yotiMockId: string, docSelectionData:any, givenName1:any, givenName2:any, givenName3:any, familyName:any ) => {
+		f2fStubPayload.yotiMockID = yotiMockId;
+		f2fStubPayload.shared_claims.name[0].nameParts[0].value = givenName1;
+		f2fStubPayload.shared_claims.name[0].nameParts[1].value = givenName2 + " " + givenName3;
+		f2fStubPayload.shared_claims.name[0].nameParts[2].value = familyName;
 
-	// 	const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
-	// 	const sessionId = sessionResponse.data.session_id;
-	// 	const sub = sessionResponse.data.sub;
+		const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
+		const sessionId = sessionResponse.data.session_id;
+		const sub = sessionResponse.data.sub;
 
-	// 	// Document Selection
-	// 	const response = await postDocumentSelection(docSelectionData, sessionId);
-	// 	expect(response.status).toBe(200);
-	// 	// Authorization
-	// 	const authResponse = await authorizationGet(sessionId);
-	// 	expect(authResponse.status).toBe(200);
-	// 	// // Post Token
-	// 	const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
-	// 	expect(tokenResponse.status).toBe(200);
-	// 	// Post User Info
-	// 	const userInfoResponse = await userInfoPost("Bearer " + tokenResponse.data.access_token);
-	// 	expect(userInfoResponse.status).toBe(202);
+		// Document Selection
+		const response = await postDocumentSelection(docSelectionData, sessionId);
+		expect(response.status).toBe(200);
+		// Authorization
+		const authResponse = await authorizationGet(sessionId);
+		expect(authResponse.status).toBe(200);
+		// // Post Token
+		const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
+		expect(tokenResponse.status).toBe(200);
+		// Post User Info
+		const userInfoResponse = await userInfoPost("Bearer " + tokenResponse.data.access_token);
+		expect(userInfoResponse.status).toBe(202);
 
-	// 	// Get Yoti Session Id
-	// 	const session = await getSessionById(sessionId, constants.DEV_F2F_SESSION_TABLE_NAME);
-	// 	const yotiSessionId: any = session?.yotiSessionId;
-	// 	console.log(yotiSessionId);
+		// Get Yoti Session Id
+		const session = await getSessionById(sessionId, constants.DEV_F2F_SESSION_TABLE_NAME);
+		const yotiSessionId: any = session?.yotiSessionId;
+		console.log(yotiSessionId);
 
-	// 	// Yoti Callback
-	// 	const callbackResponse = await callbackPost(yotiSessionId);
-	// 	console.log(callbackResponse.data);
+		// Yoti Callback
+		const callbackResponse = await callbackPost(yotiSessionId);
+		console.log(callbackResponse.data);
 
-	// 	// Retrieve Verifiable Credential from dequeued SQS queue
-	// 	let sqsMessage;
-	// 	do {
-	// 		sqsMessage = await getDequeuedSqsMessage(sub);
-	// 	} while (!sqsMessage);
-	// 	const jwtToken = sqsMessage["https://vocab.account.gov.uk/v1/credentialJWT"][0];
-	// 	validateJwtTokenNamePart(jwtToken, givenName1, givenName2, givenName3, familyName + yotiMockId);
-	// }, 20000);
+		// Retrieve Verifiable Credential from dequeued SQS queue
+		let sqsMessage;
+		do {
+			sqsMessage = await getDequeuedSqsMessage(sub);
+		} while (!sqsMessage);
+		const jwtToken = sqsMessage["https://vocab.account.gov.uk/v1/credentialJWT"][0];
+		validateJwtTokenNamePart(jwtToken, givenName1, givenName2, givenName3, familyName + yotiMockId);
+	}, 20000);
 
-	// it("F2F CRI Callback Endpoint TxMA Validation", async () => {
-	// 	f2fStubPayload.yotiMockID = "0000";
-	// 	const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
-	// 	const sessionId = sessionResponse.data.session_id;
-	// 	const sub = sessionResponse.data.sub;
+	it("F2F CRI Callback Endpoint TxMA Validation", async () => {
+		f2fStubPayload.yotiMockID = "0000";
+		const sessionResponse = await startStubServiceAndReturnSessionId(f2fStubPayload);
+		const sessionId = sessionResponse.data.session_id;
+		const sub = sessionResponse.data.sub;
 
-	// 	// Document Selection
-	// 	const response = await postDocumentSelection(dataPassport, sessionId);
-	// 	expect(response.status).toBe(200);
-	// 	// Authorization
-	// 	const authResponse = await authorizationGet(sessionId);
-	// 	expect(authResponse.status).toBe(200);
-	// 	// // Post Token
-	// 	const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
-	// 	expect(tokenResponse.status).toBe(200);
-	// 	// Post User Info
-	// 	const userInfoResponse = await userInfoPost("Bearer " + tokenResponse.data.access_token);
-	// 	expect(userInfoResponse.status).toBe(202);
+		// Document Selection
+		const response = await postDocumentSelection(dataPassport, sessionId);
+		expect(response.status).toBe(200);
+		// Authorization
+		const authResponse = await authorizationGet(sessionId);
+		expect(authResponse.status).toBe(200);
+		// // Post Token
+		const tokenResponse = await tokenPost(authResponse.data.authorizationCode.value, authResponse.data.redirect_uri );
+		expect(tokenResponse.status).toBe(200);
+		// Post User Info
+		const userInfoResponse = await userInfoPost("Bearer " + tokenResponse.data.access_token);
+		expect(userInfoResponse.status).toBe(202);
 
-	// 	// Get Yoti Session Id
-	// 	const session = await getSessionById(sessionId, constants.DEV_F2F_SESSION_TABLE_NAME);
-	// 	const yotiSessionId: any = session?.yotiSessionId;
-	// 	console.log(yotiSessionId);
+		// Get Yoti Session Id
+		const session = await getSessionById(sessionId, constants.DEV_F2F_SESSION_TABLE_NAME);
+		const yotiSessionId: any = session?.yotiSessionId;
+		console.log(yotiSessionId);
 
-	// 	// Yoti Callback
-	// 	const callbackResponse = await callbackPost(yotiSessionId);
-	// 	expect(callbackResponse.status).toBe(202);
+		// Yoti Callback
+		const callbackResponse = await callbackPost(yotiSessionId);
+		expect(callbackResponse.status).toBe(202);
 
-	// 	// Retrieve Verifiable Credential from dequeued SQS queue
-	// 	let sqsMessage;
-	// 	do {
-	// 		sqsMessage = await getSqsEventList("txma/", sessionId, 6);
-	// 	} while (!sqsMessage);
-	// 	await validateTxMAEventData(sqsMessage);
+		// Retrieve Verifiable Credential from dequeued SQS queue
+		let sqsMessage;
+		do {
+			sqsMessage = await getSqsEventList("txma/", sessionId, 6);
+		} while (!sqsMessage);
+		await validateTxMAEventData(sqsMessage);
 
-	// }, 20000);
+	}, 20000);
 });
