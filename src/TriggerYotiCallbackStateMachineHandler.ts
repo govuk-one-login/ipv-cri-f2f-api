675b77af5b4c55df668354300947a5c8bf237245
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2023-09-08 13:07:21 +0100

    TriggerYotiCallbackStateMachineHandler handles decision

--- src/TriggerYotiCallbackStateMachineHandler.ts
+++ src/TriggerYotiCallbackStateMachineHandler.ts
@@ -15,13 +15,45 @@ const logger = new Logger({
 	serviceName: POWERTOOLS_SERVICE_NAME,
 });
 
-// const metrics = new Metrics({ namespace: POWERTOOLS_METRICS_NAMESPACE, serviceName: POWERTOOLS_SERVICE_NAME });
+const metrics = new Metrics({ namespace: POWERTOOLS_METRICS_NAMESPACE, serviceName: POWERTOOLS_SERVICE_NAME });
 
 class TriggerYotiCallbackStateMachineHandler implements LambdaInterface {
 	// @metrics.logMetrics({ throwOnEmptyMetrics: false, captureColdStartMetric: true })
-	handler(event: SQSEvent, context: any): void {
-    logger.info("hi");
+	handler(event: SQSEvent, context: any): string | null {
 
+		// clear PersistentLogAttributes set by any previous invocation, and add lambda context for this invocation
+		logger.setPersistentLogAttributes({});
+		logger.addContext(context);
+
+		if (event.Records.length === 1) {
+			const record: SQSRecord = event.Records[0];
+			logger.debug("Starting to process record", { record });
+
+			const body = JSON.parse(record.body);
+			logger.appendKeys({
+				yotiSessionId: body.session_id,
+			});
+
+			logger.debug("Parsed SQS event body", body);
+
+			if (body.topic === "session_completion" || body.topic ===  "thank_you_email_requested") {
+				logger.info("Matched topic, triggering state machine");
+				return body;
+
+			} else {
+				logger.warn("Unexpected topic received in request", {
+					topic: body.topic,
+					messageCode: MessageCodes.UNEXPECTED_VENDOR_MESSAGE,
+				});
+				return null;
+			}
+		} else {
+			logger.warn("Unexpected no of records received", {
+				messageCode: MessageCodes.INCORRECT_BATCH_SIZE,
+			});
+			return null;
+
+		}
 	}
 }
 
