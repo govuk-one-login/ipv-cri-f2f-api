0349df51d5aeefd4aa7e58aa9cf528cbc16085d8
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-09-18 12:07:00 +0100

    wip

--- f2f-ipv-stub/src/handlers/callback.ts
+++ f2f-ipv-stub/src/handlers/callback.ts
@@ -3,15 +3,8 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  if (event.queryStringParameters?.error != null) {
-    return {
-      statusCode: 200,
-      body: "Authorisation Grant Failed. See logs for details",
-    };
-  } else {
-    return {
-      statusCode: 200,
-      body: "Authorization Granted. See logs for details",
-    };
-  }
+	return {
+		statusCode: 200,
+		body: JSON.stringify(event),
+	};
 };
