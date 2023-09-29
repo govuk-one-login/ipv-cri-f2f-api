a7adc06a787eac91065552cb2eace147c1eb21d8
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2023-09-29 08:54:55 +0100

    QA updates for KIWI-827

--- src/tests/api/DocumentSelectionNegativePath.test.ts
+++ src/tests/api/DocumentSelectionNegativePath.test.ts
@@ -1,8 +1,40 @@
 import dataDriversLicenseInvalid from "../data/docSelectionPayloadDriversLicenceInvalid.json";
 import dataPassport from "../data/docSelectionPayloadPassportValid.json";
-import { postDocumentSelection, startStubServiceAndReturnSessionId, authorizationGet, tokenPost, userInfoPost } from "../utils/ApiTestSteps";
+import { postDocumentSelection, startStubServiceAndReturnSessionId, authorizationGet, tokenPost, userInfoPost, stubStartPost, sessionPost } from "../utils/ApiTestSteps";
 import f2fStubPayload from "../data/exampleStubPayload.json";
 import { constants } from "../utils/ApiConstants";
+
+describe("E2E Negative Path /session Endpoint", () => {
+	let sessionId: any;
+	beforeEach(async () => {
+		f2fStubPayload.yotiMockID = "0000";
+	});
+
+	it("E2E Negative Path Journey - No Given Name Field", async () => {
+		f2fStubPayload.shared_claims.name[0].nameParts[0].value = "";
+		f2fStubPayload.shared_claims.name[0].nameParts[1].value = "";
+		const stubResponse = await stubStartPost(f2fStubPayload);
+		const sessionResponse = await sessionPost(stubResponse.data.clientId, stubResponse.data.request);
+		expect(sessionResponse.status).toBe(401);
+		expect(sessionResponse.data).toEqual("Unauthorized");
+	});
+
+	it("E2E Happy Path Journey - No Family Name Field", async () => {
+		f2fStubPayload.shared_claims.name[0].nameParts[2].value = "";
+		const stubResponse = await stubStartPost(f2fStubPayload);
+		const sessionResponse = await sessionPost(stubResponse.data.clientId, stubResponse.data.request);
+		expect(sessionResponse.status).toBe(401);
+		expect(sessionResponse.data).toEqual("Unauthorized");
+	});
+
+	it("E2E Happy Path Journey - No Email Address", async () => {
+		f2fStubPayload.shared_claims.emailAddress = "";
+		const stubResponse = await stubStartPost(f2fStubPayload);
+		const sessionResponse = await sessionPost(stubResponse.data.clientId, stubResponse.data.request);
+		expect(sessionResponse.status).toBe(401);
+		expect(sessionResponse.data).toEqual("Unauthorized");
+	});
+});
 
 
 describe("E2E Negative Path /documentSelection Endpoint", () => {
