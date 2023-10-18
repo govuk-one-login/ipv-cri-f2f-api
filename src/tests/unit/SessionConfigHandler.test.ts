c76711c0dd7fca3c08abee8760038c578262eab0
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2023-10-18 22:48:14 +0100

    PR Comments

--- src/tests/unit/SessionConfigHandler.test.ts
+++ src/tests/unit/SessionConfigHandler.test.ts
@@ -1,37 +1,24 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEvent } from "aws-lambda";
 import { Constants } from "../../utils/Constants";
 import { lambdaHandler } from "../../SessionConfigHandler";
 import { mock } from "jest-mock-extended";
-import { UNSUPPORTED_SESSION_CONFIG, VALID_SESSION_CONFIG } from "./data/session-config-events";
-import { Logger } from "@aws-lambda-powertools/logger";
-import { Metrics } from "@aws-lambda-powertools/metrics";
 
-import { Response } from "../../utils/Response";
 import { AppError } from "../../utils/AppError";
-import { HttpCodesEnum } from "../../utils/HttpCodesEnum";
 import { SessionConfigRequestProcessor } from "../../services/SessionConfigRequestProcessor";
-
-// jest.mock("@aws-lambda-powertools/logger");
-// jest.mock("@aws-lambda-powertools/metrics");
-
-const mockLogger = mock<Logger>();
-const mockMetrics = new Metrics({ namespace: "F2F" });
+import { VALID_SESSION_CONFIG } from "./data/session-config-events";
 
 const mockSessionConfigRequestProcessor = mock<SessionConfigRequestProcessor>();
 
 describe("SessionConfigHandler", () => {
-	let event: APIGatewayProxyEvent;
-
-	beforeEach(() => {
-		event = {
-			requestContext: { requestId: "sampleRequestId" },
-			headers: {},
-		} as APIGatewayProxyEvent;
-	});
+	let event = {
+		requestContext: { requestId: "sampleRequestId" },
+		headers: {},
+	} as APIGatewayProxyEvent;
 
 	afterEach(() => {
 		jest.clearAllMocks();
 	});
+
 
 	it("should return BAD_REQUEST when sessionId header is missing", async () => {
 		const response = await lambdaHandler(event, {});
@@ -49,37 +36,26 @@ describe("SessionConfigHandler", () => {
 	});
 
 	it("should call processRequest when sessionId is valid", async () => {
-		event.headers = {
-			[Constants.X_SESSION_ID]: "f47ac10b-58cc-4372-a567-0e02b2c3d479",
-		};
 		SessionConfigRequestProcessor.getInstance = jest.fn().mockReturnValue(mockSessionConfigRequestProcessor);
-		await lambdaHandler(event, {});
+		await lambdaHandler(VALID_SESSION_CONFIG, {});
 		expect(mockSessionConfigRequestProcessor.processRequest).toHaveBeenCalledTimes(1);
 	});
 
 	it("should return custom AppError when thrown from the processor", async () => {
-		event.headers = {
-			[Constants.X_SESSION_ID]: "f47ac10b-58cc-4372-a567-0e02b2c3d479",
-		};
-        
 		const customError = new AppError(500, "Random Error");
 		mockSessionConfigRequestProcessor.processRequest.mockRejectedValueOnce(customError);
 		SessionConfigRequestProcessor.getInstance = jest.fn().mockReturnValue(mockSessionConfigRequestProcessor);
 
-		const response = await lambdaHandler(event, {});
+		const response = await lambdaHandler(VALID_SESSION_CONFIG, {});
 		expect(response.statusCode).toBe(500);
 		expect(response.body).toBe("Random Error");
 	});
 
 	it("should return SERVER_ERROR when any unexpected error occurs", async () => {
-		event.headers = {
-			[Constants.X_SESSION_ID]: "f47ac10b-58cc-4372-a567-0e02b2c3d479",
-		};
-
 		mockSessionConfigRequestProcessor.processRequest.mockRejectedValueOnce(new Error("Unexpected error"));
 		SessionConfigRequestProcessor.getInstance = jest.fn().mockReturnValue(mockSessionConfigRequestProcessor);
 
-		const response = await lambdaHandler(event, {});
+		const response = await lambdaHandler(VALID_SESSION_CONFIG, {});
 		expect(response.statusCode).toBe(500);
 		expect(response.body).toBe("SessionConfigProcessor encoundered an error.");
 	});
