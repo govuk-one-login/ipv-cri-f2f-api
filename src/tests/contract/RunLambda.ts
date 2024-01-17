3368ba579506a351d211cc84e12cc68f7c63fc6a
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-01-17 16:17:09 +0000

    Package.json commands working

--- src/tests/contract/RunLambda.ts
+++ src/tests/contract/RunLambda.ts
@@ -4,9 +4,6 @@ import express from "express";
 export const authRouter = express.Router();
 
 authRouter.post("/", (req, res) => {
-	console.log("============================================");
-	console.log("req", req.body);
-
 	const response = lambdaHandler(req.body, {});
 	res.send(response);
 });
