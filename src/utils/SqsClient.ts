d0a853ec7ed5eaa8ec53f533a1b66232d6fc0c41
Bhavana <bhavana.hemanth@digital.cabinet-office.gov.uk>
2024-01-24 13:54:05 +0000

    Refactoring dynamoDB and SQSClient for contract testing

--- src/utils/SqsClient.ts
+++ src/utils/SqsClient.ts
@@ -1,18 +1,29 @@
 import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 import { NodeHttpHandler } from "@aws-sdk/node-http-handler";
 import AWSXRay from "aws-xray-sdk-core";
+import { mockSqsCient } from "../tests/contract/mocks/sqsClient";
 
-AWSXRay.setContextMissingStrategy("LOG_ERROR");
+const createSqsClient = () => {
+	let sqsClient: SQSClient;
+	if (process.env.USE_MOCKED) {
+		console.log("USING MOCKED");
+		sqsClient = mockSqsCient as unknown as SQSClient;
+	} else {
 
-const sqsClientRaw = new SQSClient({
-	region: process.env.REGION,
-	maxAttempts: 2,
-	requestHandler: new NodeHttpHandler({
-		connectionTimeout: 29000,
-		socketTimeout: 29000,
-	}),
-});
+		AWSXRay.setContextMissingStrategy("LOG_ERROR");
 
-const sqsClient = process.env.XRAY_ENABLED === "true" ? AWSXRay.captureAWSv3Client(sqsClientRaw as any) : sqsClientRaw;
+		const sqsClientRaw = new SQSClient({
+			region: process.env.REGION,
+			maxAttempts: 2,
+			requestHandler: new NodeHttpHandler({
+				connectionTimeout: 29000,
+				socketTimeout: 29000,
+			}),
+		});
+		sqsClient = process.env.XRAY_ENABLED === "true" ? AWSXRay.captureAWSv3Client(sqsClientRaw as any) : sqsClientRaw;
 
-export { sqsClient, SendMessageCommand };
+	}
+	return sqsClient;
+};
+
+export { createSqsClient, SendMessageCommand };
