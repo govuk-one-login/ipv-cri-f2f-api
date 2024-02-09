3cf77c9048e83b3c6ceafd53eb05b036a83aa591
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2024-02-09 09:32:22 +0000

    Lint fixes

--- src/tests/api/CallbackApi.test.ts
+++ src/tests/api/CallbackApi.test.ts
@@ -135,7 +135,7 @@ describe("Callback API", () => {
 				sqsMessage = await getDequeuedSqsMessage(sessionResponse.data.sub);
 				await sleep(1000);
 				i++;
-			} while (i < 10 || sqsMessage == undefined);
+			} while (i < 10 || sqsMessage === undefined);
 	
 			expect(sqsMessage.error_description).toBe(vcError);
 		}, 20000);
