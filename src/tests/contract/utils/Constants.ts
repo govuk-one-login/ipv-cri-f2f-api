df4ed6046ae218f9b27fcbccbafe28ec90bb2f5d
Bhavana <115095929+hemavana@users.noreply.github.com>
2024-02-19 17:17:11 +0000

    [KIWI-1556]F2F- Write contract tests for identified API integration points (#343)
    * Refactoring dynamoDB and SQSClient for contract testing
    
    * WIP: Updating KMS, SSM clients in the code and unit-tests.
    
    * Commenting unused function (it required some sqsclient re refactoring)
    
    * linting
    
    * pr comments
    
    * Contract testing core code to setup DB tables and start app and mock authorization server and test.
    
    * secrets file
    
    * WIP: Adding AccessToken mock server route
    
    * Renaming the test file.
    
    * WIP: Adding test
    
    * Refactoring the setup-dynamodb script to use batch-write-item
    
    * Updating the KMS mock for tests.
    
    * secrets file
    
    * secrets file
    
    * Adding UserInfo dummy server code.
    
    * WIP: Contract Testing- Verifier code checkin
    
    * Removing Authorization mock server and tests.
    
    * Adding loggers.
    
    * package-lock file updates
    
    * updates package-lock file
    
    * Removing the unwanted db record item.
    
    * Workaround for request body translation
    
    * further refactoring
    
    * refactoring
    
    * PR comments
    
    * PR comments

---
+++ src/tests/contract/utils/Constants.ts
@@ -0,0 +1,15 @@
+export class Constants {
+    
+    static readonly TOKEN_ENDPOINT = "/token";
+
+    static readonly USERINFO_ENDPOINT = "/userinfo";
+
+    static readonly LOCAL_APP_PORT = 3000;
+
+    static readonly HTTP_CONTENT_TYPE_HEADER = "Content-Type";
+
+    static readonly JSON_CONTENT_TYPE = "application/json";
+
+    static readonly LOCAL_HOST = "http://localhost";
+
+}
