e2bd2e97f8436f802444311e330cb9c6bac628b1
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2024-04-16 12:34:07 +0100

    Try via axios

--- src/tests/contract/F2fCriProvider.test.ts
+++ src/tests/contract/F2fCriProvider.test.ts
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/tslint/config */
+/* eslint-disable max-lines-per-function */
 import { Logger } from "@aws-lambda-powertools/logger";
 import { Verifier, VerifierOptions } from "@pact-foundation/pact";
 import { Constants } from "./utils/Constants";
+import axios from "axios";
+import btoa from "btoa";
 
 const logger = new Logger({
 	logLevel: "INFO",
@@ -16,7 +20,7 @@ describe("Pact Verification", () => {
 			provider: process.env.PACT_PROVIDER_NAME,
 			// we need to where the provider will be running,
 			// we are starting it locally and defined the port above
-			// providerBaseUrl: `${Constants.LOCAL_HOST}:${Constants.LOCAL_APP_PORT}`,
+			providerBaseUrl: `${Constants.LOCAL_HOST}:${Constants.LOCAL_APP_PORT}`,
 			pactBrokerUrl: process.env.PACT_BROKER_URL,
 			pactBrokerUsername: process.env.PACT_BROKER_USER,
 			pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
@@ -29,13 +33,40 @@ describe("Pact Verification", () => {
 			// You can set the log level here, useful for debugging
 			logLevel: "debug",
 		};
+
+		const auth = btoa(`${opts.pactBrokerUsername}:${opts.pactBrokerPassword}`);
+		const pact_url = opts.pactBrokerUrl || "";
+
+		axios.get(pact_url, {
+			headers: {
+				"Authorization": `Basic ${auth}`,
+			},
+		})
+			.then(function (response) {
+				console.log("PACT BROKER AUTHORIZED SUCCESSFULLY VIA PROVIDER");
+				return response.status;
+			})
+			.catch(function (error) {
+				if (error.response) {
+					console.log("ERROR AUTHORIZING PACT BROKER:", error.response.status);
+					throw error.response;
+				} else if (error.request) {
+					console.log("ERROR WITH REQUEST MADE TO PACT BROKER");
+					throw error.request;
+				} else {
+					console.log("ERROR SETTING UP REQUEST TO PACT BROKER:", error.message);
+					throw error;
+				}
+			});
 	});  
   
 	it("should validate the expectations of Authorization API", async () => {
 		logger.debug("PactBroker opts: ", { opts });
 		let result;
-		await new Verifier(opts).verifyProvider();
-			// .verifyProvider()
+
+
+		// await new Verifier(opts).verifyProvider();
+		// .verifyProvider()
 		// 	.then((output) => {
 		// 		logger.info("Pact Verification Complete!");
 		// 		logger.info("Output: ", output);
