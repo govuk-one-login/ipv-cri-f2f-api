61508d3cf0d50e36ca04cc8f48378488ff6ea495
Bhavana <bhavana.hemanth@digital.cabinet-office.gov.uk>
2024-04-16 16:55:45 +0100

    Uncommenting the unit and infra tests.

--- src/tests/contract/scripts/test-pact-connection.sh
+++ src/tests/contract/scripts/test-pact-connection.sh
@@ -6,6 +6,7 @@ password="$PACT_BROKER_PASSWORD"
 pact_url="$PACT_BROKER_URL"
 
 # Perform a GET request with Basic Authentication using curl
+# Required to connect to PactBroker as Pact libraries don't allow testSource parameter to be passed in TS
 response=$(curl -s -o /dev/null -w "%{http_code}" --user "$username:$password" "$pact_url")
 
 # Check the HTTP status code of the response
