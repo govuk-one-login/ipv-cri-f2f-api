b43b9949b473e975cfb24f192f41ec9dcf7a745f
Bhavana <bhavana.hemanth@digital.cabinet-office.gov.uk>
2024-04-16 17:14:02 +0100

    updating the log level back to info.

--- src/tests/contract/F2fCriProvider.test.ts
+++ src/tests/contract/F2fCriProvider.test.ts
@@ -30,7 +30,7 @@ describe("Pact Verification", () => {
 			publishVerificationResult: true,
 			providerVersion: process.env.PACT_PROVIDER_VERSION,
 			// You can set the log level here, useful for debugging
-			logLevel: "debug",
+			logLevel: "info",
 		};
 	});  
 	
