3ab42412e1d7207f4541b40c677a95a4365e03e2
Viveak <viveak.vadivelkarasan@digital.cabinet-office.gov.uk>
2024-04-16 13:26:43 +0100

    Connect to pact using curl

--- src/tests/contract/scripts/test-pact-connection.sh
+++ src/tests/contract/scripts/test-pact-connection.sh
@@ -1,56 +1,20 @@
 #!/bin/bash
 
-# Define your constants in the bash script if necessary
+# Define environment variables
 export LOCAL_HOST="localhost"
 export LOCAL_APP_PORT="3000"
 
-# Run Node.js code directly from the bash script
-node <<EOF
-const axios = require('axios');
+# Read environment variables for authentication
+username="$PACT_BROKER_USER"
+password="$PACT_BROKER_PASSWORD"
+pact_url="$PACT_BROKER_URL"
 
-// Access environment variables
-const opts = {
-  provider: process.env.PACT_PROVIDER_NAME,
-  providerBaseUrl: \`\${process.env.LOCAL_HOST}:\${process.env.LOCAL_APP_PORT}\`,
-  pactBrokerUrl: process.env.PACT_BROKER_URL,
-  pactBrokerUsername: process.env.PACT_BROKER_USER,
-  pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
-  consumerVersionSelectors: [
-    { mainBranch: true },
-    { deployedOrReleased: true }
-  ],
-  publishVerificationResult: true,
-  providerVersion: process.env.PACT_PROVIDER_VERSION,
-  logLevel: "debug"
-};
+# Perform a GET request with Basic Authentication using curl
+response=$(curl -s -o /dev/null -w "%{http_code}" --user "$username:$password" "$pact_url")
 
-const auth = Buffer.from(\`\${opts.pactBrokerUsername}:\${opts.pactBrokerPassword}\`).toString("base64");
-const pact_url = opts.pactBrokerUrl || "";
-
-axios.get(pact_url, {
-  headers: {
-    "Authorization": \`Basic \${auth}\`
-  }
-})
-.then(function (response) {
-  if (response.status === 200) {
-    console.log("PACT BROKER AUTHORIZED SUCCESSFULLY VIA PROVIDER");
-    return response.status;
-  } else {
-    console.log("Response code:", response.status);
-  }
-})
-.catch(function (error) {
-  if (error.response) {
-    console.log("ERROR AUTHORIZING PACT BROKER:", error.response.status);
-    throw error.response;
-  } else if (error.request) {
-    console.log("ERROR WITH REQUEST MADE TO PACT BROKER");
-    throw error.request;
-  } else {
-    console.log("ERROR SETTING UP REQUEST TO PACT BROKER:", error.message);
-    throw error;
-  }
-});
-EOF
-
+# Check the HTTP status code of the response
+if [ "$response" -eq 200 ]; then
+    echo "PACT BROKER AUTHORIZED SUCCESSFULLY VIA PROVIDER"
+else
+    echo "Response code: $response"
+fi
