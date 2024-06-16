55c3784dd082bd5d978745e9332717dba92d946b
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2024-06-16 18:17:35 +0100

    pdf pref and postal address sent to govnotify

--- src/utils/GovNotifyEvent.ts
+++ src/utils/GovNotifyEvent.ts
@@ -10,8 +10,8 @@ export interface GovNotifyEvent {
 		"firstName": string;
 		"lastName": string;
 		"messageType": string;
-		"letterPreference": string;
-		"postalAddress": object;
+		"pdfPreference": string;
+		"postalAddress"?: PersonIdentityAddress;
 	};
 }
 
@@ -34,19 +34,42 @@ export interface ReminderEmailEventDynamic {
 
 export const buildGovNotifyEventFields = (sessionId: string, yotiSessionId: string, personDetails: PersonIdentityItem): GovNotifyEvent => {
 	const nameParts = personIdentityUtils.getNames(personDetails);
-
-	return {
-		Message : {
-			sessionId,
-			yotiSessionId,
-			emailAddress: personIdentityUtils.getEmailAddress(personDetails),
-			firstName: nameParts.givenNames[0],
-			lastName: nameParts.familyNames[0],
-			messageType: Constants.PDF_EMAIL,
-			letterPreference: personIdentityUtils.getLetterPreference(personDetails),
-			postalAddress: personIdentityUtils.getPostalAddress(personDetails),
-		},
-	};
+	if (personDetails.pdfPreference === "letter") {
+		const addressesLength = personDetails.addresses.length;
+		return {
+			Message : {
+				sessionId,
+				yotiSessionId,
+				emailAddress: personIdentityUtils.getEmailAddress(personDetails),
+				firstName: nameParts.givenNames[0],
+				lastName: nameParts.familyNames[0],
+				messageType: Constants.PDF_EMAIL,
+				pdfPreference: personIdentityUtils.getpdfPreference(personDetails),
+				postalAddress: {
+					uprn: personDetails.addresses[addressesLength - 1].uprn,
+					subBuildingName: personDetails.addresses[addressesLength - 1].subBuildingName,
+					buildingNumber: personDetails.addresses[addressesLength - 1].buildingNumber,
+					buildingName: personDetails.addresses[addressesLength - 1].buildingName,
+					streetName: personDetails.addresses[addressesLength - 1].streetName,
+					addressLocality: personDetails.addresses[addressesLength - 1].addressLocality,
+					postalCode: personDetails.addresses[addressesLength - 1].postalCode,
+					addressCountry: personDetails.addresses[addressesLength - 1].addressCountry,
+				},
+			},
+		};
+	} else {
+		return {
+			Message : {
+				sessionId,
+				yotiSessionId,
+				emailAddress: personIdentityUtils.getEmailAddress(personDetails),
+				firstName: nameParts.givenNames[0],
+				lastName: nameParts.familyNames[0],
+				messageType: Constants.PDF_EMAIL,
+				pdfPreference: personIdentityUtils.getpdfPreference(personDetails),
+			},
+		};
+	}
 };
 
 export const buildReminderEmailEventFields = (emailAddress: string): ReminderEmailEvent => {
