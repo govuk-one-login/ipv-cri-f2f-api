718539c205cfb6b769a2a1245bf07e2c5589d968
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2024-06-17 14:38:04 +0100

    Test fixes and making pdfPreference optional property on PersonIdentityItem interface

--- src/tests/unit/services/DocumentSelectionRequestProcessor.test.ts
+++ src/tests/unit/services/DocumentSelectionRequestProcessor.test.ts
@@ -92,6 +92,7 @@ function getPersonIdentityItem(): PersonIdentityItem {
 				],
 			},
 		],
+		"pdfPreference": "email",
 		expiryDate: 1612345678,
 		createdDate: 1612335678,
 	};
@@ -208,8 +209,7 @@ describe("DocumentSelectionRequestProcessor", () => {
 
 		mockYotiService.generateInstructions.mockResolvedValueOnce(HttpCodesEnum.OK);
 
-		const out: APIGatewayProxyResult = await mockDocumentSelectionRequestProcessor.processRequest(VALID_REQUEST, "RandomF2FSessionID", encodedHeader);
-
+		const out: Response = await mockDocumentSelectionRequestProcessor.processRequest(VALID_REQUEST, "RandomF2FSessionID", encodedHeader);
 		expect(mockF2fService.sendToTXMA).toHaveBeenCalledTimes(1);
 		const passportYotiStart = TXMA_PASSPORT_YOTI_START;
 		passportYotiStart.event_name = "F2F_YOTI_START";
@@ -224,7 +224,6 @@ describe("DocumentSelectionRequestProcessor", () => {
 
 	it("Should return successful response with 200 OK when non-UK passport used for YOTI session", async () => {
 		yotiSessionInfo.capture.required_resources[0].supported_countries[0].code = "ESP";
-
 		mockF2fService.getSessionById.mockResolvedValueOnce(f2fSessionItem);
 		mockF2fService.getPersonIdentityById.mockResolvedValueOnce(personIdentityItem);
 
