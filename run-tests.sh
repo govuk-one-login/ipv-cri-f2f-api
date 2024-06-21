49c5d1537bc7945a0f3038deca23a79f14a9e7d5
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2024-06-21 11:56:30 +0100

    run-tests.sh updates

--- run-tests.sh
+++ run-tests.sh
@@ -12,7 +12,7 @@ cd /src
 if [ "$SAM_STACK_NAME" == "f2f-yoti-stub" ]; then
   export DEV_F2F_YOTI_STUB_URL=$(remove_quotes "$CFN_F2FYotiStubURL")
   npm run test:yoti
-  error_code=$?
+  test_yoti_error_code=$?
 elif [ "$SAM_STACK_NAME" == "f2f-cri-api" ]; then
   echo "SAM_STACK_NAME is f2f-cri-api"
   
@@ -25,14 +25,24 @@ elif [ "$SAM_STACK_NAME" == "f2f-cri-api" ]; then
   export DNS_SUFFIX=$(remove_quotes "$CFN_DNSSuffix")
   export DEV_F2F_SESSION_TABLE_NAME=$(remove_quotes "$CFN_SessionTableName")
   
-  npm run test:api 
-  error_code=$?
+  npm run test:api
+  test_api_error_code=$?
+  
+  npm run test:api-third-party
+  test_api_third_party_error_code=$?
+  
+  # Combine error codes to ensure script fails if either test fails
+  if [ $test_api_error_code -ne 0 ] || [ $test_api_third_party_error_code -ne 0 ]; then
+    error_code=1
+  else
+    error_code=0
+  fi
 else 
   echo "No BackEnd Regression Tests being Ran"
   exit 0
 fi
 
-# disabling error_check to allow report generation for successful + failed tests
+# Disabling error_check to allow report generation for successful + failed tests
 cp -rf results $TEST_REPORT_ABSOLUTE_DIR
 if [ $error_code -ne 0 ]; then
   exit $error_code
