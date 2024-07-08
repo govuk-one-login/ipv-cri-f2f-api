31f09c3c6e74ddc752f94cfb78ffe6f386e971a1
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2024-07-08 10:39:33 +0100

    Bump aws cdk to V2 for L2 stacks

--- infra-l2-outbound-proxy/src/lib/test-helper.ts
+++ infra-l2-outbound-proxy/src/lib/test-helper.ts
@@ -1,4 +1,4 @@
-import { Template } from "@aws-cdk/assertions";
+import { Template } from 'aws-cdk-lib/assertions';
 import { readFileSync } from "fs";
 import { load } from "js-yaml";
 import { schema } from "yaml-cfn";
