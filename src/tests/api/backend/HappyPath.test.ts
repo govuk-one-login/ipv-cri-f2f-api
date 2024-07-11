54152c5bbfe0601935acdd4dffa530af640cb4fa
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2024-07-11 12:18:16 +0100

    Removed tests that used "PRINTED_LETTER" pdfPreference as this is unsupported by current FE hardcoded value "EMAIL_ONLY"

--- src/tests/api/backend/HappyPath.test.ts
+++ src/tests/api/backend/HappyPath.test.ts
@@ -19,8 +19,6 @@ import thinFilePayload from "../../data/thinFilePayload.json";
 import abortPayload from "../../data/abortPayload.json";
 import dataPassport from "../../data/docSelectionPayloadPassportValid.json";
 import dataUkDrivingLicence from "../../data/docSelectionPayloadDriversLicenceValid.json";
-import dataUkDrivingLicencePrintedLetter from "../../data/docSelectionPayloadDriversLicenceValidPrintedLetter.json";
-import dataUkDrivingLicencePreferredAddress from "../../data/docSelectionPayloadDriversLicenceValidPreferredAddress.json";
 import dataEuDrivingLicence from "../../data/docSelectionPayloadEuDriversLicenceValid.json";
 import dataNonUkPassport from "../../data/docSelectionPayloadNonUkPassportValid.json";
 import dataBrp from "../../data/docSelectionPayloadBrpValid.json";
@@ -112,52 +110,6 @@ describe("/documentSelection Endpoint", () => {
 			console.error("Error validating PDF Preference from Person Identity Table", error);
 			throw error;
 		}
-	});
-
-	it.each([
-		{ docSelectionData: dataUkDrivingLicencePrintedLetter },
-	])("Successful Request Tests - $PrintedLetter", async ({ docSelectionData }) => {
-		const newf2fStubPayload = structuredClone(f2fStubPayload);
-		const { sessionId } = await startStubServiceAndReturnSessionId(newf2fStubPayload);
-
-
-		const postResponse = await postDocumentSelection(docSelectionData, sessionId);
-		expect(postResponse.status).toBe(200);
-
-		const personIdentityRecord = await getPersonIdentityRecordById(sessionId, constants.DEV_F2F_PERSON_IDENTITY_TABLE_NAME);
-		try {
-			expect(personIdentityRecord?.pdfPreference).toBe(dataUkDrivingLicence.pdf_preference);
-		} catch (error) {
-			console.error("Error validating PDF Preference from Person Identity Table", error);
-			throw error;
-		}
-	});
-
-	it.each([
-		{ docSelectionData: dataUkDrivingLicencePreferredAddress },
-	])("Successful Request Tests - $PreferredAddress", async ({ docSelectionData }) => {
-		const newf2fStubPayload = structuredClone(f2fStubPayload);
-		const { sessionId } = await startStubServiceAndReturnSessionId(newf2fStubPayload);
-
-
-		const docSelect = structuredClone(docSelectionData);
-		docSelect.postal_address.preferredAddress = true;
-		const postResponse = await postDocumentSelection(docSelectionData, sessionId);
-		expect(postResponse.status).toBe(200);
-
-		const personIdentityRecord = await getPersonIdentityRecordById(sessionId, constants.DEV_F2F_PERSON_IDENTITY_TABLE_NAME);
-
-		try {
-			expect(personIdentityRecord?.pdfPreference).toBe(docSelectionData.pdf_preference);
-			const preferredAddress = personIdentityRecord?.addresses?.find(address => address.preferredAddress);
-			expect(preferredAddress).toBeDefined();
-			expect(preferredAddress?.postalCode).toBe(docSelectionData.postal_address.postalCode);
-			expect(preferredAddress?.preferredAddress).toBe(true);
-		} catch (error) {
-			console.error("Error validating PDF and Address Preference from Person Identity Table", error);
-			throw error;
-		}
-
 	});
 });
 
