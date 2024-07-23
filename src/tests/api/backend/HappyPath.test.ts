630ad5fdfe8353c7517d04f8eec004ef2b02ce82
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2024-07-23 12:42:05 +0100

    lint fixes

--- src/tests/api/backend/HappyPath.test.ts
+++ src/tests/api/backend/HappyPath.test.ts
@@ -271,7 +271,7 @@ describe("/sessionConfiguration endpoint", () => {
 		const sessionConfigurationResponse = await sessionConfigurationGet(sessionId);
 
 		expect(sessionConfigurationResponse.status).toBe(200);
-		expect(sessionConfigurationResponse.data.pcl_enabled).toEqual("true");
+		expect(sessionConfigurationResponse.data.pcl_enabled).toBe("true");
 		expect(sessionConfigurationResponse.data.evidence_requested.strengthScore).toEqual(strengthScore);
 	});
 
@@ -283,7 +283,7 @@ describe("/sessionConfiguration endpoint", () => {
 		const sessionConfigurationResponse = await sessionConfigurationGet(sessionId);
 
 		expect(sessionConfigurationResponse.status).toBe(200);
-		expect(sessionConfigurationResponse.data).not.toHaveProperty("evidence_requested")
+		expect(sessionConfigurationResponse.data).not.toHaveProperty("evidence_requested");
 	});
 });
 
