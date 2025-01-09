282f6ee7d7dfcebbfad4b4e37642cf6509320297
Madan Karuppiah <madan.karuppiah@digital.cabinet-office.gov.uk>
2025-01-09 17:25:25 +0000

    Put test harness on top and run the tests

--- traffic-tests/run-tests.sh
+++ traffic-tests/run-tests.sh
@@ -48,17 +48,14 @@ case "$SAM_STACK_NAME" in
 
         export DEV_F2F_PERSON_IDENTITY_TABLE_NAME=$(remove_quotes "$CFN_PersonIdentityTableName")
 
-        npm run "test:api-traffic" &
         npm run "test:api-third-party-traffic" &
+        npm run "test:api-traffic" &
         sleep 10
         npm run "test:api-traffic" &
-        npm run "test:api-third-party-traffic" &
         sleep 10
         npm run "test:api-traffic" &
-        npm run "test:api-third-party-traffic" &
         sleep 10
         npm run "test:api-traffic" &
-        npm run "test:api-third-party-traffic" &
         wait
 
         error_code=$?
