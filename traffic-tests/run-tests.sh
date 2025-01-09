7bbe663343f914e3da11c78108d78474498044b8
Madan Karuppiah <madan.karuppiah@digital.cabinet-office.gov.uk>
2025-01-09 12:15:50 +0000

    Test traffic test parallel without running error codes

--- traffic-tests/run-tests.sh
+++ traffic-tests/run-tests.sh
@@ -48,13 +48,17 @@ case "$SAM_STACK_NAME" in
 
         export DEV_F2F_PERSON_IDENTITY_TABLE_NAME=$(remove_quotes "$CFN_PersonIdentityTableName")
 
-        run_tests "test:api-traffic" "test:api-third-party-traffic" &
+        npm run "test:api-traffic" &
+        npm run "test:api-third-party-traffic" &
         sleep 10
-        run_tests "test:api-traffic" "test:api-third-party-traffic" &
+        npm run "test:api-traffic" &
+        npm run "test:api-third-party-traffic" &
         sleep 10
-        run_tests "test:api-traffic" "test:api-third-party-traffic" &
+        npm run "test:api-traffic" &
+        npm run "test:api-third-party-traffic" &
         sleep 10
-        run_tests "test:api-traffic" "test:api-third-party-traffic" &
+        npm run "test:api-traffic" &
+        npm run "test:api-third-party-traffic" &
         wait
 
         error_code=$?
