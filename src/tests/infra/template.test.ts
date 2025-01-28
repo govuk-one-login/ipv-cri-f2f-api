018fc56408a98c863b1891ff7ec57039b6436dcf
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2025-01-28 11:58:32 +0000

    WIP

--- src/tests/infra/template.test.ts
+++ src/tests/infra/template.test.ts
@@ -61,8 +61,8 @@ describe("Infra", () => {
     });
   });
 
-  it("There are 19 lambdas defined, all with at least one specific permission:", () => {
-    const lambdaCount = 19;
+  it("There are 20 lambdas defined, all with at least one specific permission:", () => {
+    const lambdaCount = 20;
     const lambdaPermissionCount = 22;
     template.resourceCountIs("AWS::Serverless::Function", lambdaCount);
     template.resourceCountIs("AWS::Lambda::Permission", lambdaPermissionCount);
--- src/tests/unit/OsApiKeyHandler.test.ts
+++ src/tests/unit/OsApiKeyHandler.test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/unbound-method */
-import { lambdaHandler, logger } from "../../OSApiKeyHandler";
+import { lambdaHandler, logger } from "../../OsApiKeyHandler";
 import { VALID_AUTHCODE } from "./data/auth-events";
 import { CONTEXT } from "./data/context";
 import { HttpCodesEnum } from "../../models/enums/HttpCodesEnum";
