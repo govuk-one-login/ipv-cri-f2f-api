45484bc52f7798148efaec1ea4dc50de3b0fe78a
GDainton <117987734+GDainton@users.noreply.github.com>
2025-05-06 14:59:36 +0100

    [KIWI-2270] - OAuth | Update IPV Core Stub to send signed JWT to /token (#796)
    Co-authored-by: Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>

---
+++ f2f-ipv-stub/src/tests/generateTokenRequest.test.ts
@@ -0,0 +1,79 @@
+import { handler } from "../handlers/generateTokenRequest";
+import {
+  expect,
+  jest,
+  it,
+  beforeEach,
+  afterEach,
+  describe,
+} from "@jest/globals";
+import { mockClient } from "aws-sdk-client-mock";
+import "aws-sdk-client-mock-jest";
+import { KMSClient, SignCommand } from "@aws-sdk/client-kms";
+import format from "ecdsa-sig-formatter";
+
+const testData = require("../events/startEvents.js");
+
+jest.setTimeout(30000);
+
+process.env.SIGNING_KEY = "key-id";
+process.env.ADDITIONAL_KEY = "additional-key-id";
+process.env.JWT_AUDIENCE = "aud";
+
+const kmsClient = mockClient(KMSClient);
+
+describe("Start F2F Check Endpoint", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(new Date(1585695600000)); // == 2020-03-31T23:00:00.000Z
+
+    kmsClient.on(SignCommand).resolves({
+      Signature: new Uint8Array([
+        197, 213, 5, 202, 58, 74, 45, 36, 122, 168, 27, 155, 70, 15, 9, 123, 11,
+        241, 205, 87, 23, 13, 32, 168, 12, 73, 48, 158, 96, 159, 247, 211,
+      ]),
+    });
+
+    jest.spyOn(format, "derToJose").mockReturnValue(
+      // pragma: allowlist nextline secret
+      "PmBhykH4w94xj3dSDSR-tE5XSh60SjKAP6hHGc6c_fx7ia87hEkKgfhSTCT000RaDhH0MaV47FsUjztCb0m1qg"
+    );
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    jest.resetAllMocks();
+  });
+
+  it("returns JWT data", async () => {
+    const response = await handler(testData.startDefault);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeDefined();
+  });
+
+  describe("Sign function", () => {
+    it("should sign the JWT using the correct key", async () => {
+      const response = await handler(testData.startDefault);
+      const signCommandInput =
+        kmsClient.commandCalls(SignCommand)[0].args[0].input;
+      expect(signCommandInput.KeyId).toBe("key-id");
+      expect(response.statusCode).toBe(200);
+    });
+
+    it("should sign a JWT using the correct key when provided with a custom payload for 'invalidKid'", async () => {
+      const response = await handler(testData.startCustomInvalidSigningKey);
+      const signCommandInput =
+        kmsClient.commandCalls(SignCommand)[0].args[0].input;
+      expect(signCommandInput.KeyId).toBe("key-id");
+      expect(response.statusCode).toBe(200);
+    });
+
+    it("should sign a JWT using the correct key when provided with a custom payload for 'missingKid'", async () => {
+      const response = await handler(testData.startCustomMissingSigningKey);
+      const signCommandInput =
+        kmsClient.commandCalls(SignCommand)[0].args[0].input;
+      expect(signCommandInput.KeyId).toBe("key-id");
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
