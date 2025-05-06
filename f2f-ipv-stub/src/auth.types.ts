59819c5dd766359425e875c2ec324e80a05c6211
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2025-05-06 11:51:54 +0100

    generate token request

--- f2f-ipv-stub/src/auth.types.ts
+++ f2f-ipv-stub/src/auth.types.ts
@@ -6,21 +6,24 @@ export interface EvidenceRequested {
   identityFraudScore?: number;
 }
 
-export interface JarPayload {
-  [key: string]: any;
-  sub: string
-  redirect_uri: string;
-  client_id: string;
-  response_type: "code";
-  state: string;
+export interface JWTPayload {
+  // Required as per OpenID Connect documentation
+  iss: string;
+  sub: string;
+  aud: string;
+  jti: string;
   exp: number;
-  nbf: number;
-  iat: number;
-  aud?: string | string[];
+  // Additional optional values
+  redirect_uri?: string;
+  client_id?: string;
+  response_type?: string;
+  state?: string;
+  nbf?: number;
+  iat?: number;
   scope?: string;
   nonce?: string;
-  iss?: string;
   evidence_requested?: EvidenceRequested | undefined;
+  [key: string]: any;
 }
 export interface Jwks {
   keys: JsonWebKey[];
