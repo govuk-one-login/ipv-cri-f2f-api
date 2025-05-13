26c6fd65a4c97bf39208b262313cfa5e3d72cd32
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2025-05-13 07:58:45 +0100

    Test

--- src/tests/contract/F2fCriProvider.test.ts
+++ src/tests/contract/F2fCriProvider.test.ts
@@ -22,7 +22,8 @@ describe("Pact Verification", () => {
 			pactBrokerUsername: process.env.PACT_BROKER_USER,
 			pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
 			consumerVersionSelectors: [
-				{ branch: "PYIC-8320-add-kid-to-contract-tests" },
+				{ mainBranch: true },
+				// { branch: "PYIC-8320-add-kid-to-contract-tests" },
 				{ deployedOrReleased: true },
 			  ],
 			publishVerificationResult: true,
