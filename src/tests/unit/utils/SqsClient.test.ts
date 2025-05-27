f874d11366ed0f65dbed5b6e127e37f71af0978d
Stelios_CB_GDS <stelios.constantinou-briggs@digital.cabinet-office.gov.uk>
2025-05-27 13:01:51 +0100

    Update unit tests

---
+++ src/tests/unit/utils/SqsClient.test.ts
@@ -0,0 +1,38 @@
+import * as AWS from "@aws-sdk/client-kms";
+import { mockSqsClient } from "../../../tests/contract/mocks/sqsClient";
+import { createKmsClient } from "../../../utils/KMSClient";
+import { Logger } from "@aws-lambda-powertools/logger";
+
+describe("createKmsClient", () => {
+	
+	let loggerSpy: jest.SpyInstance;
+
+	beforeEach(() => {
+		loggerSpy = jest.spyOn(Logger.prototype, 'info');
+	});
+
+    afterEach(() => {
+		loggerSpy.mockRestore();
+        jest.clearAllMocks();
+    });
+
+	it("should use the mocked client and log correctly when USE_MOCKED is true", () => {
+        process.env.USE_MOCKED = "true";
+
+        const kmsClient = createKmsClient();
+
+        expect(kmsClient).toBe(mockSqsClient);
+        expect(loggerSpy).toHaveBeenCalledWith("KMSClient: USING MOCKED");
+    });
+
+    it("should return a real KMS client when USE_MOCKED is not true or undefined", () => {
+        process.env.USE_MOCKED = undefined;
+        process.env.REGION = "eu-west-2";
+        process.env.XRAY_ENABLED = "false";
+
+        const kmsClient = createKmsClient();
+        expect(kmsClient).toBeInstanceOf(AWS.KMS);
+    });
+
+});
+
