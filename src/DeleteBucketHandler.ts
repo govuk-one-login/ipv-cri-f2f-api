c8f5657dd487b6e86fda2c433d2ccd372379fb06
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2025-11-26 13:56:53 +0000

    if statement checking request type moved to handler

--- src/DeleteBucketHandler.ts
+++ src/DeleteBucketHandler.ts
@@ -4,13 +4,15 @@ import { DeleteBucketProcessor } from "./services/DeleteBucketProcessor";
 import { Response } from "./utils/Response";
 
 export class DeleteBucketHandler implements LambdaInterface {
-    async handler(event: any): Promise<any> {
-        try {
-            return await DeleteBucketProcessor.getInstance().processRequest(event);
-        } catch {
-            return Response(HttpCodesEnum.SERVER_ERROR, "An error has occured")
-        }
+  async handler(event: any): Promise<any> {
+    if (event.RequestType === "Delete") {
+      try {
+        return await DeleteBucketProcessor.getInstance().processRequest(event);
+      } catch {
+          return Response(HttpCodesEnum.SERVER_ERROR, "An error has occured")
+      }
     }
+  }
 }
 
 const handlerClass = new DeleteBucketHandler();
