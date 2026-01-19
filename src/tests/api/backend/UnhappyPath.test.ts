0b8da05cc849d2d47a32e42650805424c81324d3
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2026-01-19 12:37:06 +0000

    //QualityGateStackTest tag added

--- src/tests/api/backend/UnhappyPath.test.ts
+++ src/tests/api/backend/UnhappyPath.test.ts
@@ -24,6 +24,7 @@ import { randomUUID } from "crypto";
 
 //QualityGateIntegrationTest 
 //QualityGateRegressionTest
+//QualityGateStackTest
 describe("Unhappy Path Tests", () => {
 
 	describe("/session endpoint", () => {
