d85950b81b39a0310d91c5911964c7a2cf110e04
Sophie Kane <sophie.kane@digital.cabinet-office.gov.uk>
2026-02-12 10:19:25 +0000

    KIWI-2571 resolving xmlparser CVE and updating aws/sdk

--- src/tests/visual/helpers.ts
+++ src/tests/visual/helpers.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
-import * as pdfjsLib from from "pdfjs-dist/legacy/build/pdf";
+import { getDocument, PDFPageProxy} from "pdfjs-dist/legacy/build/pdf.js";
 import { createCanvas, Canvas } from "canvas";
 
 /**
