71a6f1b8cfe0dcfdab237a6fd133d8a295a86457
Sophie Kane <sophie.kane@digital.cabinet-office.gov.uk>
2026-05-28 09:51:16 +0100

    KIWI-2642 small pre-merge fix

--- src/tests/unit/utils/SqsClient.test.ts
+++ src/tests/unit/utils/SqsClient.test.ts
@@ -3,11 +3,13 @@ import * as AWS from "@aws-sdk/client-sqs";
 import { mockSqsClient } from "../../../tests/contract/mocks/sqsClient";
 import { createSqsClient } from "../../../utils/SqsClient";
 import { Logger } from "@aws-lambda-powertools/logger";
-import { captureAWSv3Client } from "aws-xray-sdk-core";
+import AWSXRay from "aws-xray-sdk-core";
 
 vi.mock("aws-xray-sdk-core", () => ({
-    captureAWSv3Client: vi.fn((client) => client),
-    setContextMissingStrategy: vi.fn(),
+    default: {
+        captureAWSv3Client: vi.fn((client) => client),
+        setContextMissingStrategy: vi.fn(),
+    },
 }));
 
 describe("createSqsClient", () => {
@@ -45,14 +47,14 @@ describe("createSqsClient", () => {
         process.env.XRAY_ENABLED = "true";
         process.env.REGION = "eu-west-2";
         createSqsClient();
-        expect(captureAWSv3Client).toHaveBeenCalledWith(expect.any(AWS.SQSClient));
+        expect(AWSXRay.captureAWSv3Client).toHaveBeenCalledWith(expect.any(AWS.SQSClient));
     });
 
     it("should return a raw SQS client when XRAY_ENABLED is false and USE_MOCKED is not set", () => {
         process.env.XRAY_ENABLED = "false";
         process.env.REGION = "eu-west-2"; // Or your desired region
         const sqsClient = createSqsClient();
-        expect(captureAWSv3Client).not.toHaveBeenCalled();
+        expect(AWSXRay.captureAWSv3Client).not.toHaveBeenCalled();
         expect(sqsClient).toBeInstanceOf(AWS.SQSClient);
     });
 
@@ -60,7 +62,7 @@ describe("createSqsClient", () => {
     it("should return a raw SQS client when XRAY_ENABLED is not set and USE_MOCKED is not set", () => {
         process.env.REGION = "eu-west-2"; // Or your desired region
         const sqsClient = createSqsClient();
-        expect(captureAWSv3Client).not.toHaveBeenCalled();
+        expect(AWSXRay.captureAWSv3Client).not.toHaveBeenCalled();
         expect(sqsClient).toBeInstanceOf(AWS.SQSClient);
     });
 
--- src/utils/DynamoDBFactory.ts
+++ src/utils/DynamoDBFactory.ts
@@ -1,9 +1,9 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import { fromEnv } from "@aws-sdk/credential-providers";
-import { captureAWSv3Client, setContextMissingStrategy } from "aws-xray-sdk-core";
+import AWSXRay from "aws-xray-sdk-core";
 
-setContextMissingStrategy("LOG_ERROR");
+AWSXRay.setContextMissingStrategy("LOG_ERROR");
 
 const awsRegion = process.env.AWS_REGION;
 export const createDynamoDbClient = () => {
@@ -20,5 +20,5 @@ export const createDynamoDbClient = () => {
 	const endpoint = useMocks ? "http://localhost:8000" : undefined;
 	const dbClient = new DynamoDBClient({ region: awsRegion, credentials: fromEnv(), endpoint });
 	const dbClientRaw = DynamoDBDocument.from(dbClient, translateConfig);
-	return process.env.XRAY_ENABLED === "true" ? captureAWSv3Client(dbClientRaw as any) : dbClientRaw;
+	return process.env.XRAY_ENABLED === "true" ? AWSXRay.captureAWSv3Client(dbClientRaw as any) : dbClientRaw;
 };
